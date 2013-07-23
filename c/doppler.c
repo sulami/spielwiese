ccdf9e036d0b9e020c0280b443217ed61360fcfe
Rob Schroer <sulamiification@gmail.com>
2013-07-23 12:53:45 +0200

    new stuff in c

--- c/doppler.c
+++ c/doppler.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int doppelt(input)
-{
-    int result = input * 2;
-    printf(result);
-    return 0;
-}
+int doppelt (int input);
 
 int main(void)
 {
-    int i = 4;
-    doppelt(i);
+    int i;
+    printf("Input number to double: ");
+    scanf("%i", &i);
+    printf("%i\n", doppelt(i));
     return 0;
+}
+
+int doppelt(int input)
+{
+    int result = input * 2;
+    return result;
 }
 
--- c/hello world.c
+++ c/hello world.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    puts("Hello, world!");
+    printf("Hello, world!\n");
     return 0;
 }
 
