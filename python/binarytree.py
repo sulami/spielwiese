bd37dfd0a3e8f5ec60d7f7f6fa4863ea93be72e1
Robin Schroer <sulamiification@gmail.com>
2014-05-03 22:26:08 +0200

    python/binarytree: form fixes

--- python/binarytree.py
+++ python/binarytree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """
 Implements binary search trees that can hold arbitrary data connected to
@@ -13,60 +13,66 @@ class Node:
         self.left = left
         self.right = right
 
-    def __get__(self, key):
+    def _get(self, key):
         """Returns Node object for a given key"""
         if key == self.key:
             return self
         if key < self.key:
             if self.left is None:
                 return None
-            return self.left.__get__(key)
+            return self.left._get(key)
         else:
             if self.right is None:
                 return None
-            return self.right.__get__(key)
+            return self.right._get(key)
 
-    def __multi_get__(self, key, results):
+    def _multi_get(self, key, results):
         """Actually fetches for multi_lookup"""
-        me = self.__get__(key)
+        me = self._get(key)
         if me is None:
             return None
-        results.append(me.data)
+        results.append(me)
         left = me.left
         if left is not None:
-            left_key = left.__get__(key)
+            left_key = left._get(key)
             if left_key is not None:
-                results.append(left_key.data)
+                results.append(left_key)
         right = me.right
         if right is not None:
-            right_key = right.__get__(key)
+            right_key = right._get(key)
             if right_key is not None:
-                results.append(right_key.data)
+                results.append(right_key)
         return results
 
-    def __ordered_get__(self, results):
+    def _ordered_get(self, results):
         """Actually fetches for ordered_lookup"""
         if self.left is not None:
-            self.left.__ordered_get__(results)
+            self.left._ordered_get(results)
         results.append((self.key, self.data))
         if self.right is not None:
-            self.right.__ordered_get__(results)
+            self.right._ordered_get(results)
         return results
 
     def lookup(self, key):
         """Returns data for the first matching key"""
-        result = self.__get__(key)
+        result = self._get(key)
         if result is not None:
             return result.data
         return None
 
     def multi_lookup(self, key):
         """Returns a list of all data matching the key"""
-        return self.__multi_get__(key, [])
+        results = self._multi_get(key, [])
+        if results is None:
+            return None
+        datas = []
+        for result in results:
+            datas.append(result.data)
+        return datas
 
     def ordered_lookup(self):
         """Returns all items ordered by key in a list of tuples"""
-        return self.__ordered_get__([])
+        return self._ordered_get([])
 
     def insert(self, key, data):
         """Inserts key-data pair into tree"""
