da4547dd35cd29a6e7ea18216416883d6d37554d
Robin Schroer <sulamiification@gmail.com>
2014-05-04 15:17:40 +0200

    c/binarytest: proper prompts

--- c/binarytest.c
+++ c/binarytest.c
@@ -5,8 +5,23 @@
 int main(int argc, const char *argv[])
 {
     char action;
+    printf("(n)ew, (l)ookup, (e)xit: ");
     while (action != 'e') {
-        scanf("%c", &action);
+        scanf(" %c", &action);
+        switch (action) {
+            case 'n':
+                printf("new node\n");
+                printf("(n)ew, (l)ookup, (e)xit: ");
+                break;
+            case 'l':
+                printf("lookup\n");
+                printf("(n)ew, (l)ookup, (e)xit: ");
+                break;
+            case 'e':
+                break;
+            default:
+                printf("(n)ew, (l)ookup, (e)xit: ");
+        }
     }
     return 0;
 }
