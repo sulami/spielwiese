04330b8f05b02cf4e7a07f362ac55167f56c5d8e
Robin Schroer <sulamiification@gmail.com>
2014-05-11 22:23:32 +0200

    added test/logic for conway's rule #1

--- cGol/gol.h
+++ cGol/gol.h
@@ -2,24 +2,59 @@
 #include <time.h>
 
 void *cgol_gen_start(unsigned int width, unsigned int height);
+int *cgol_next_gen(int *screen, unsigned int width, unsigned int height);
 int cgol_live(void);
 
 void *cgol_gen_start(unsigned int width, unsigned int height) {
     int (*screen)[height][width] = malloc(sizeof(int) * width * height);
     if (screen != NULL) {
         srand((unsigned int)time(NULL));
-        unsigned int i;
-        for (i=0; i<height; i++) {
-            unsigned int j;
-            for (j=0; j<width; j++) {
-                int randint = rand() % 2;
-                (*screen)[i][j] = randint;
+        for (unsigned int i=0; i<height; i++) {
+            for (unsigned int j=0; j<width; j++) {
+                (*screen)[i][j] = rand() % 2;
             }
         }
         return(screen);
     } else {
         return(NULL);
     }
+}
+
+int *cgol_next_gen(int *screen, unsigned int width, unsigned int height) {
+    int *tmp = malloc(sizeof(int) * width * height);
+    for (unsigned int i=0; i<height; i++) {
+        for (unsigned int j=0; j<width; j++) {
+            int neighbours_alive = 0;
+            if (i > 0 && j > 0) {
+                *(screen + (i - 1) * width + j - 1) ? neighbours_alive++ : 0 ;
+            }
+            if (i > 0) {
+                *(screen + (i - 1) * width + j) ? neighbours_alive++ : 0 ;
+            }
+            if (i > 0 && j < (width - 1)) {
+                *(screen + (i - 1) * width + j + 1) ? neighbours_alive++ : 0 ;
+            }
+            if (j > 0) {
+                *(screen + i * width + j - 1) ? neighbours_alive++ : 0 ;
+            }
+            if (j < (width - 1)) {
+                *(screen + i * width + j + 1) ? neighbours_alive++ : 0 ;
+            }
+            if (i < (height - 1) && j > 0) {
+                *(screen + (i + 1) * width + j - 1) ? neighbours_alive++ : 0 ;
+            }
+            if (i < (height - 1)) {
+                *(screen + (i + 1) * width + j) ? neighbours_alive++ : 0 ;
+            }
+            if (i < (height - 1) && j < (width - 1)) {
+                *(screen + (i + 1) * width + j + 1) ? neighbours_alive++ : 0 ;
+            }
+            if (neighbours_alive < 2) {
+                *(tmp + i * width + j) = 0;
+            }
+        }
+    }
+    return(tmp);
 }
 
 int cgol_live() {
--- cGol/gol_test.c
+++ cGol/gol_test.c
@@ -9,6 +9,7 @@
 void test_exits_gracefully(void);
 void test_creates_random_start(void);
 void test_values_are_zero_ore_none(void);
+void test_conways_rule_one(void);
 
 void test_exits_gracefully() {
     assert(cgol_live() == 0);
@@ -22,13 +23,30 @@ void test_values_are_zero_ore_none() {
     unsigned int x = 50;
     unsigned int y = 50;
     int *screen = cgol_gen_start(y, x);
-    unsigned int i;
-    for (i=0; i<y; i++) {
-        unsigned int j;
-        for (j=0; j<x; j++) {
+    for (unsigned int i=0; i<y; i++) {
+        for (unsigned int j=0; j<x; j++) {
             int test = *(screen + (i * j) + j);
             assert(test == 0 || test == 1);
         }
+    }
+}
+
+void test_conways_rule_one() {
+    /* Any live cell with fewer than two live neighbours dies */
+    int *screen = malloc(sizeof(int) * 9);
+    if (screen != NULL) {
+        *screen = 0;
+        *(screen + 1) = 0;
+        *(screen + 2) = 0;
+        *(screen + 3) = 0;
+        *(screen + 4) = 1;
+        *(screen + 5) = 0;
+        *(screen + 6) = 0;
+        *(screen + 7) = 0;
+        *(screen + 8) = 0;
+        int *new = cgol_next_gen(screen, 3, 3);
+        assert(*(new + 4) == 0);
+        free(screen);
     }
 }
 
@@ -37,6 +55,7 @@ int main() {
     run_test(test_exits_gracefully);
     run_test(test_creates_random_start);
     run_test(test_values_are_zero_ore_none);
+    run_test(test_conways_rule_one);
     printf("\n => All tests successful!\n");
     return(0);
 }
