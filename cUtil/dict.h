4f5ccfbada1ebc82f692cc25c868b5c9fa0c8ed7
Robin Schroer <sulamiification@gmail.com>
2014-05-25 17:34:58 +0200

    cUtil: added dictionary header (untested)

--- cUtil/dict.h
+++ cUtil/dict.h
@@ -1,61 +1,61 @@
-/* Dictionary-like data structure in C */
+/*
+ * Dictionary-like data structure in C
+ *
+ * Derived from an example in The C Programming Language (K&R)
+ */
 
 #include <string.h>
 
-struct nlist { /* table entry: */
-    struct nlist *next; /* next entry in chain */
+struct dictentry {
+    struct dictentry *next;
     char *key;
     char *content;
 };
 
-unsigned hash(char *s);
-struct nlist *lookup(struct nlist *table, char *s);
-struct nlist *install(struct nlist *table, char *key, char *content);
-char *strdup(char *);
-
 #define HASHSIZE 101
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
 
-/* hash: form hash value for string s */
-unsigned hash(char *s) {
+unsigned hash(unsigned char *s)
+{
     unsigned hashval;
     for (hashval = 0; strcmp(s, "\0"); s++)
-      hashval = (unsigned)*s + 31 * hashval;
+        hashval = (unsigned)*s + 31 * hashval;
     return hashval % HASHSIZE;
 }
 
-/* lookup: look for s in hashtab */
-struct nlist *lookup(struct nlist *table, char *s) {
-    struct nlist *np;
+struct dictentry *lookup(struct dictentry *table, unsigned char *s)
+{
+    struct dictentry *np;
     for (np = &table[hash(s)]; np != NULL; np = np->next)
         if (strcmp(s, np->key) == 0)
-          return np; /* found */
+            return np; /* found */
     return NULL; /* not found */
 }
 
-/* install: put (key, content) in hashtab */
-struct nlist *install(struct nlist *table, char *key, char *content) {
-    struct nlist *np;
+struct dictentry *install(struct dictentry *table, unsigned char *key,
+                          char *content)
+{
+    struct dictentry *np;
     unsigned hashval;
     if ((np = lookup(table, key)) == NULL) { /* not found */
         np = (struct nlist *) malloc(sizeof(*np));
         if (np == NULL || (np->key = strdup(key)) == NULL)
-          return NULL;
+            return NULL;
         hashval = hash(key);
         np->next = &table[hashval];
         table[hashval] = *np;
     } else /* already there */
         free((void *) np->content); /*free previous content */
     if ((np->content = strdup(content)) == NULL)
-       return NULL;
+        return NULL;
     return np;
 }
 
-char *strdup(char *s) { /* make a duplicate of s */
+char *strdup(unsigned char *s)
+{
     char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
+    p = (char *) malloc(strlen(s) + 1);
     if (p != NULL)
-       strcpy(p, s);
+        strcpy(p, s);
     return p;
 }
 
