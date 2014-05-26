0dfcfa7c4659a61d87c7251e693a72c5a091cbfd
Robin Schroer <sulamiification@gmail.com>
2014-05-26 03:28:13 +0200

    ir.c: added basic command line options

--- cIRC/ir.c
+++ cIRC/ir.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-#define VERSION "0.1"
-#define DEFAULTSERVER "irc.freenode.net"
-#define DEFAULTPORT 6667
+#define VERSION         "0.1"
+#define HELP            "Options:\n--version\n-s <server>\n-p <port>\n-u <user>"
+#define DEFAULTSERVER   "irc.freenode.net"
+#define DEFAULTPORT     6667
+#define DEFAULTUSER     "sulami"
 
 int main(int argc, char *argv[])
 {
+    static char *server, *user;
+    static unsigned int port;
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
-            if (!strcmp(argv[i], "--version")) {
-                printf("%s\n", VERSION);
+            if (!strcmp(argv[i], "-s")) {
+                i++;
+                server = argv[i];
+            } else if (!strcmp(argv[i], "-p")) {
+                i++;
+                port = (unsigned int)argv[i];
+            } else if (!strcmp(argv[i], "-u")) {
+                i++;
+                user = argv[i];
+            } else if (!strcmp(argv[i], "--version")) {
+                printf("IR.c version %s\n", VERSION);
+                return 0;
+            } else if (!strcmp(argv[i], "--help")) {
+                printf("%s\n", HELP);
                 return 0;
             }
         }
