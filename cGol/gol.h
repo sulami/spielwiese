87a1a9c20b1d4cce4a89f005da6bb29165951b68
Robin Schroer <sulamiification@gmail.com>
2014-06-01 23:50:43 +0200

    cGOL: added wrapping support

--- cGol/gol.h
+++ cGol/gol.h
@@ -26,30 +26,82 @@ int *cgol_next_gen(int *screen, unsigned int width, unsigned int height) {
     for (unsigned int i=0; i<height; i++) {
         for (unsigned int j=0; j<width; j++) {
             int neighbours_alive = 0;
+
+            /* top left */
             if (i > 0 && j > 0) {
                 *(screen + (i - 1) * width + j - 1) ? neighbours_alive++ : 0 ;
+            } else if (i > 0) { /* left border */
+                *(screen + i * width) ? neighbours_alive++ : 0 ;
+            } else if (j > 0) { /* top border */
+                *(screen + (height - 1) * width + j - 1) ?
+                    neighbours_alive++ : 0 ;
+            } else { /* top left corner */
+                *(screen + height * width - 1) ? neighbours_alive++ : 0 ;
             }
+
+            /* top */
             if (i > 0) {
                 *(screen + (i - 1) * width + j) ? neighbours_alive++ : 0 ;
+            } else { /* top border */
+                *(screen + (height - 1) * width + j) ? neighbours_alive++ : 0 ;
             }
+
+            /* top right */
             if (i > 0 && j < (width - 1)) {
                 *(screen + (i - 1) * width + j + 1) ? neighbours_alive++ : 0 ;
+            } else if (i > 0) { /* right border */
+                *(screen + (i - 1) * width) ? neighbours_alive++ : 0 ;
+            } else if (j < (width -1)) { /* top border */
+                *(screen + (height - 1) * width + j + 1) ?
+                    neighbours_alive++ : 0 ;
+            } else { /* top right corner */
+                *(screen + height * width - width) ? neighbours_alive++ : 0 ;
             }
+
+            /* left */
             if (j > 0) {
                 *(screen + i * width + j - 1) ? neighbours_alive++ : 0 ;
+            } else { /* left border */
+                *(screen + i * width + width) ? neighbours_alive++ : 0 ;
             }
+
+            /* right */
             if (j < (width - 1)) {
                 *(screen + i * width + j + 1) ? neighbours_alive++ : 0 ;
+            } else { /* right border */
+                *(screen + i * width) ? neighbours_alive++ : 0 ;
             }
+
+            /* bottom left */
             if (i < (height - 1) && j > 0) {
                 *(screen + (i + 1) * width + j - 1) ? neighbours_alive++ : 0 ;
+            } else if (i < (height - 1)) { /* left border */
+                *(screen + (i + 1) * width + width - 1) ?
+                    neighbours_alive++ : 0 ;
+            } else if (j > 0) { /* bottom border */
+                *(screen + j - 1) ? neighbours_alive++ : 0 ;
+            } else { /* bottom left corner */
+                *(screen + width - 1) ? neighbours_alive++ : 0 ;
             }
+
+            /* bottom */
             if (i < (height - 1)) {
                 *(screen + (i + 1) * width + j) ? neighbours_alive++ : 0 ;
+            } else { /* bottom border */
+                *(screen + i) ? neighbours_alive++ : 0 ;
             }
+
+            /* bottom right */
             if (i < (height - 1) && j < (width - 1)) {
                 *(screen + (i + 1) * width + j + 1) ? neighbours_alive++ : 0 ;
+            } else if (i < (height - 1)) { /* right border */
+                *(screen + (i + 1) * width) ? neighbours_alive++ : 0 ;
+            } else if (j < (width - 1)) { /* bottom border */
+                *(screen + j + 1) ? neighbours_alive++ : 0 ;
+            } else { /* bottom right corner */
+                *(screen) ? neighbours_alive++ : 0 ;
             }
+
             if (neighbours_alive == 2 && *(screen + i * width + j)) {
                 *(tmp + i * width + j) = 1;
             } else if (neighbours_alive == 3) {
