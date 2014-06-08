7ff1f3eaecf81512e1e40420482b3349fab59975
Robin Schroer <sulamiification@gmail.com>
2014-06-08 13:01:07 +0200

    stuff: added dtdns update script

---
+++ stuff/dtdns.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+getip=$(curl http://myip.dnsomatic.com --silent)
+path="/tmp/dtdns"
+user="a123qwertz567"
+pass="bla"
+
+if [[ -f $path ]]; then
+    old=$(cat $path)
+else
+    old="(None)"
+fi
+
+if [ "$getip" == "$old" ]; then
+    echo "IP unchanged: $old <> $getip"
+else
+    echo -e "GET /api/autodns.cfm?id=$user&pw=$pass HTTP/1.1\n\
+Host: www.dtdns.com\n\
+User-Agent: bash\n\n" |
+    nc www.dtdns.com 80 > /dev/null
+    echo "$getip" > $path
+    echo "IP updated: $old -> $getip"
+fi
+
