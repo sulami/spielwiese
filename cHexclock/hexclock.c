0a62dbd5cf3321bc13806440ccd9ace5687324a2
Robin Schroer <sulamiification@gmail.com>
2014-06-20 13:38:51 +0200

    added fake color support (no actual hex colors)

--- cHexclock/hexclock.c
+++ cHexclock/hexclock.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[]) {
     nodelay(stdscr, true);
     curs_set(FALSE);
 
+    if(!has_colors() || !can_change_color())
+        exit(1);
+
+    start_color();
+
     while(1) {
         if (getch() == 'q') {
             endwin();
@@ -27,6 +32,10 @@ int main(int argc, char *argv[]) {
         strftime(h, 3, "%H", tt);
         strftime(m, 3, "%M", tt);
         strftime(s, 3, "%S", tt);
+
+        init_color(COLOR_BLACK, atoi(h) * 10, atoi(m) * 10, atoi(s) * 10);
+        init_pair(1, COLOR_WHITE, COLOR_BLACK);
+        attron(COLOR_PAIR(1));
 
         getmaxyx(stdscr, max_y, max_x);
 
