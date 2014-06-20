bd68c81f835c4373db39848fc7a94a399dc6f804
Robin Schroer <sulamiification@gmail.com>
2014-06-20 13:10:09 +0200

    cHexclock: added time output

--- cHexclock/hexclock.c
+++ cHexclock/hexclock.c
@@ -1,9 +1,14 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     unsigned int max_y, max_x;
+    time_t t;
+    struct tm *tt;
+    char h[3], m[3], s[3];
 
     initscr();
     noecho();
@@ -17,9 +22,19 @@ int main(int argc, char *argv[]) {
             exit(0);
         }
 
+        time(&t);
+        tt = localtime(&t);
+        strftime(h, 3, "%H", tt);
+        strftime(m, 3, "%M", tt);
+        strftime(s, 3, "%S", tt);
+
         getmaxyx(stdscr, max_y, max_x);
 
         clear();
+        mvprintw(max_y / 2, max_x / 2 - 3, "#");
+        mvprintw(max_y / 2, max_x / 2 - 2, h);
+        mvprintw(max_y / 2, max_x / 2, m);
+        mvprintw(max_y / 2, max_x / 2 + 2, s);
         refresh();
 
         usleep(30000);
