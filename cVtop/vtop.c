00b6dcd8256bd96e4bd1291863d355e6a9378efe
Robin Schroer <sulamiification@gmail.com>
2014-07-09 18:03:45 +0200

    cVtop: added all sorts of string manipulation stuff to read from /proc/stats

--- cVtop/vtop.c
+++ cVtop/vtop.c
@@ -3,17 +3,82 @@
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+#include <assert.h>
 #include <stdio.h>
 
 #define BUFFERSIZE 256
+#define STATLEN 128
+
+char **str_split(char* a_str, const char a_delim)
+{
+    char **result    = 0;
+    size_t count     = 0;
+    char *tmp        = a_str;
+    char *last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp) {
+        if (a_delim == *tmp) {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char *) * count);
+
+    if (result) {
+        size_t idx  = 0;
+        char *token = strtok(a_str, delim);
+
+        while (token) {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        /* assert(idx == count - 1); */
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+static unsigned long idle(void)
+{
+    char *cpustats = calloc(sizeof(char), STATLEN),
+         **tmp = malloc(sizeof(char) * STATLEN);
+    unsigned long retval;
+    FILE *stat = fopen("/proc/stat", "r");
+    int b = 0, e;
+
+    fgets(cpustats, STATLEN, stat);
+    fclose(stat);
+
+    tmp = str_split(cpustats, ' ');
+    retval = strtoul(*(tmp + 4), 0, 10);
+
+    return retval;
+}
 
 int main(int argc, char *argv[])
 {
-    int max_x, max_y, ch,
-        cpu_time_now, cpu_time, cpu_time_old, *cpu_hist, cpu_count = 0;
-    long cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    int max_x, max_y, ch, cpu_count = 0;
+    unsigned long cpu, cpu_old = 0;
+    unsigned long *cpu_hist;
+    unsigned long cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
-    cpu_hist = calloc(sizeof(int), BUFFERSIZE);
+    cpu_hist = calloc(sizeof(long), BUFFERSIZE);
     if (!cpu_hist)
         exit(1);
 
@@ -34,11 +99,9 @@ int main(int argc, char *argv[])
             }
         }
 
-        cpu_time_now = clock();
-        cpu_time = cpu_time_old ? cpu_time_now - cpu_time_old : 0;
-        cpu_time_old = cpu_time_now;
-
-        cpu_hist[cpu_count] = cpu_time / cpus;
+        cpu = idle();
+        cpu_hist[cpu_count] = (cpu - cpu_old) / cpus;
+        cpu_old = cpu;
 
         getmaxyx(stdscr, max_y, max_x);
         clear();
