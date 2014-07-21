159e67c992548f6c62d0abba18af5d5ac85d96ec
Robin Schroer <sulamiification@gmail.com>
2014-07-21 02:46:40 +0200

    ccc/bcmoo: add padding prototype, basic cbc structure

--- cCryptoCat/bcmoo.h
+++ cCryptoCat/bcmoo.h
@@ -2,12 +2,27 @@
  * Blcok cipher modes of operation for use with my crappy ciphers
  */
 
+#include <errno.h>
+#include <string.h>
 #include "xor.h"
 
 char *enc_cbc(char *in, unsigned int cipher, unsigned int bs, char *iv);
 
 #define CIPHER_NULL   0
 #define CIPHER_XOR    1
+
+/*
+ * Add padding to plaintext blocks according to PKCS7 (RFC 5652), filling up to
+ * a whole block with the amount of bytes padding added.
+ *
+ * TAKES:
+ * char *in:        input string
+ * unsigned int bs: blocksize
+ */
+static void pad_blocks(char *in, unsigned int bs)
+{
+    /* TODO: realloc *in, add the padding */
+}
 
 /*
  * CBC mode - XOR the IV with the first plainblock, then the cipherblocks with
@@ -21,6 +36,22 @@ char *enc_cbc(char *in, unsigned int cipher, unsigned int bs, char *iv);
  */
 char *enc_cbc(char *in, unsigned int cipher, unsigned int bs, char *iv)
 {
+    char *out;
+
+    if (!in || !bs || !iv)
+        return (char *)-EINVAL;
+
+    if (strlen(in) % bs)
+        pad_blocks(in, bs);
+
+    out = malloc(sizeof(in));
+    if (!out)
+        return (char *)-ENOMEM;
+
+    for (unsigned int i = 0; i < strlen(in) / bs; i++) {
+        /* TODO: xor iv/cipherblocks, send them to the proper cipher */
+    }
+
     return in;
 }
 
