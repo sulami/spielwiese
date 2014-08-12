d6e39f291bcd057ebe635426eac029285f587bcb
Robin Schroer <sulamiification@gmail.com>
2014-08-12 11:15:18 +0200

    pyUtil/multicam: use function parameters...

--- pyUtil/multitable.py
+++ pyUtil/multitable.py
@@ -1,6 +1,6 @@
 def table(a=12, b=12):
-    for i in xrange(12):
-        for j in xrange(12):
+    for i in xrange(a):
+        for j in xrange(b):
             print "%3d" % ((i + 1) * (j + 1)),
         print
 
