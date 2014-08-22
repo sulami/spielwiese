fa373638a6b025e38c1afcf617b1c128bb11e850
Robin Schroer <sulamiification@gmail.com>
2014-08-22 19:34:08 +0200

    dBTree: add support for removing child nodes

--- dBTree/btree.d
+++ dBTree/btree.d
@@ -4,6 +4,7 @@ class Node
 {
     uint key;
     uint data;
+    Node p;
     Node l;
     Node r;
 
@@ -13,19 +14,62 @@ class Node
         this.data = data;
     }
 
+    Node lookup(uint key)
+    {
+        if (key == this.key)
+            return this;
+        if (key < this.key && this.l !is null)
+            return this.l.lookup(key);
+        if (this.r !is null)
+            return this.r.lookup(key);
+        return null;
+    }
+
     void insert(uint key, typeof(Node.data) data)
     {
         if (key <= this.key) {
-            if (this.l is null)
+            if (this.l is null) {
                 this.l = new Node(key, data);
-            else
+                this.l.p = this;
+            } else {
                 this.l.insert(key, data);
+            }
         } else {
-            if (this.r is null)
+            if (this.r is null) {
                 this.r = new Node(key, data);
-            else
+                this.r.p = this;
+            } else {
                 this.r.insert(key, data);
+            }
         }
+    }
+
+    void remove(uint key)
+    {
+        Node node = this.lookup(key);
+        if (node is null)
+            return;
+        Node parent = node.p;
+        bool leftshort = (node._left_path() <= node._right_path());
+        bool isleft = (parent.l == node);
+        if (node.l !is null && node.r !is null) {
+            /* relocate both */
+            isleft ? parent.l : parent.r = leftshort ? node.l : node.r;
+            Node tmp = parent;
+            while ((isleft ? tmp.r : tmp.l) !is null)
+                tmp = isleft ? tmp.l : tmp.r;
+            isleft ? tmp.r : tmp.l = leftshort ? node.r : node.l;
+        } else if (node.l !is null) {
+            /* relocate just left */
+            isleft ? parent.l : parent.r = node.l;
+        } else if (node.r !is null) {
+            /* relocate just right */
+            isleft ? parent.l : parent.r = node.r;
+        } else {
+            /* nothing to relocate */
+            isleft ? parent.l : parent.r = null;
+        }
+        destroy(node);
     }
 
     uint size()
@@ -38,15 +82,18 @@ class Node
         return retval;
     }
 
-    Node lookup(uint key)
+    protected uint _left_path()
     {
-        if (key == this.key)
-            return this;
-        if (key < this.key && this.l !is null)
-            return this.l.lookup(key);
         if (this.r !is null)
-            return this.r.lookup(key);
-        return null;
+            return (1 + this.r._left_path());
+        return 0;
+    }
+
+    protected uint _right_path()
+    {
+        if (this.l !is null)
+            return (1 + this.l._right_path());
+        return 0;
     }
 }
 
@@ -65,5 +112,8 @@ unittest {
     assert(root.lookup(6).data == 36);
     assert(root.lookup(15) is null);
     assert(root.size() == 7);
+    root.remove(5);
+    assert(root.size() == 6);
+    assert(root.lookup(5) is null);
 }
 
