912e86a015e2db1874636bc107bb5abe13b3c642
Robin Schroer <sulami@peerwire.org>
2014-11-10 23:36:25 +0100

    OneDimension: add enemy ai

--- cOneDimension/onedimension.c
+++ cOneDimension/onedimension.c
@@ -2,10 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static int max_x, max_y, ch, x;
+#define SIZE 8
+#define DAMAGE 25
+#define SPAWN_CHANCE 5
+#define SPEED 20
+#define ENEMY_HP 100
+
+#define RANDOM rand() % 100
+
+struct enemy {
+    unsigned int x;
+    bool right;
+    int hp;
+    struct enemy *next;
+};
+
+static int max_x, max_y, ch, x, size;
+static struct enemy *enemies;
+
+struct enemy *spawn_enemy(bool right)
+{
+    struct enemy *e = malloc(sizeof(struct enemy));
+    if (!e)
+        return 0;
+
+    e->x = right ? max_x : 1;
+    e->right = right;
+    e->hp = ENEMY_HP;
+    e->next = NULL;
+
+    return e;
+}
 
 void event_loop()
 {
+    struct enemy *e;
+
     ch = getch();
     if (ch) {
         switch(ch) {
@@ -22,9 +54,27 @@ void event_loop()
     }
 
     getmaxyx(stdscr, max_y, max_x);
-
     clear();
+
     mvprintw(max_y / 2, x, "O");
+    mvprintw(max_y / 2, x - size, "|");
+    mvprintw(max_y / 2, x + size, "|");
+
+    for (e = enemies; e; e = e->next) {
+        if (RANDOM <= SPEED)
+            e->x = e->right ? e->x - 1 : e->x + 1;
+        mvprintw(max_y / 2, e->x, "X");
+    }
+
+    if (RANDOM * 10 <= SPAWN_CHANCE) {
+        if (enemies) {
+            for (e = enemies; e->next; e = e->next);
+            e->next = spawn_enemy(RANDOM > 50 ? false : true);
+        } else {
+            enemies = spawn_enemy(RANDOM > 50 ? false : true);
+        }
+    }
+
     refresh();
 }
 
@@ -37,6 +87,7 @@ int main(int argc, char *argv[])
     curs_set(FALSE);
     getmaxyx(stdscr, max_y, max_x);
 
+    size = SIZE;
     x = max_x / 2;
 
     while(1) {
