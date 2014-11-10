af8af220c3b604b7a21eb35357c1945fadf549f6
Robin Schroer <sulami@peerwire.org>
2014-11-10 22:44:21 +0100

    cOneDimension: adjust routines for game

--- cOneDimension/onedimension.c
+++ cOneDimension/onedimension.c
@@ -1,37 +1,50 @@
 #include <ncurses.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    initscr();
-    noecho();
-    curs_set(FALSE);
+static int max_x, max_y, ch, x;
 
-    int x = 0;
-    int y = 0;
-    int max_x = 0;
-    int max_y = 0;
-    int direction_x = 1;
-    int direction_y = 1;
+void event_loop()
+{
+    ch = getch();
+    if (ch) {
+        switch(ch) {
+        case 'z':
+            x -= 1;
+            break;
+        case 'm':
+            x += 1;
+            break;
+        case 'q':
+            endwin();
+            exit(0);
+        }
+    }
+
+    getmaxyx(stdscr, max_y, max_x);
+
+    clear();
+    mvprintw(max_y / 2, x, "O");
+    refresh();
+}
+
+int main(int argc, char *argv[])
+{
+    initscr();
+    raw();
+    noecho();
+    nodelay(stdscr, true);
+    curs_set(FALSE);
+    getmaxyx(stdscr, max_y, max_x);
+
+    x = max_x / 2;
 
     while(1) {
-        clear();
-        mvprintw(y, x, "O");
-        refresh();
-
-        x = x + direction_x;
-        y = y + direction_y;
-
-        getmaxyx(stdscr, max_y, max_x);
-        if (x >= max_x || x < 0) {
-            direction_x *= -1;
-        }
-        if (y >= max_y || y < 0) {
-            direction_y *= -1;
-        }
-
+        event_loop();
         usleep(30000);
     }
 
     endwin();
+    return 0;
 }
 
