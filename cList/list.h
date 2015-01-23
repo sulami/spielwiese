9ec244da5c2d5e2f55f6e6a1f5186e3b308f8dad
Robin Schroer <sulami@peerwire.org>
2015-01-23 09:59:33 +0100

    cList: add some documentation

--- cList/list.h
+++ cList/list.h
@@ -10,9 +10,31 @@ struct list_head {
     void *payload;
 };
 
+/*
+ * list_add - create a new list or append to an existing one
+ *
+ * Passing NULL as list_head * will result in creation of a new list
+ * which will be returned. Takes the payload of the new element as
+ * second argument.
+ */
 struct list_head *list_add(struct list_head *, void *);
+
+/*
+ * list_remove - remove an item from a list
+ *
+ * Pass the list root element as well as the list_head * of the element
+ * to be removed.
+ */
 struct list_head *list_remove(struct list_head *, struct list_head *);
+
+/*
+ * list_delete - delete an entire list
+ */
 void list_delete(struct list_head *);
+
+/*
+ * list_length - returns the length of a list
+ */
 unsigned int list_length(struct list_head *);
 
 #endif /* LIST_H */
