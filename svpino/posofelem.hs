949c8db7cbc2297c26ea247d3a0adcf82f99c280
Robin Schroer <sulami@peerwire.org>
2015-05-25 20:43:38 +0200

    some minor tweaks

--- svpino/posofelem.hs
+++ svpino/posofelem.hs
@@ -1,5 +1,5 @@
--- Playing around with SVPINO's post about finding the position of an element
--- in an array. Related post:
+-- Playing around with svpino's post about finding the position of an element
+-- in a list. Related post:
 -- https://blog.svpino.com/2015/05/24/programming-challenge-the-position-of-the-element
 
 -- This is the obvious solution that won't scale well with large arrays, O(n).
@@ -7,6 +7,9 @@ obvPos :: Int -> [Int] -> Maybe Int
 obvPos n l = find n l 0
   where
     find n [] c = Nothing
-    find n l c | n == head l = Just c
-               | otherwise = find n (tail l) (c+1)
+    find n l  c | n == head l = Just c
+                |   otherwise = find n (tail l) (c+1)
+
+-- Other idea: Try splitting the list in halfs to reduce the number of
+-- comparisons down to O(log n).
 
