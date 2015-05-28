ba87e24c98b423827e54ed2a3a74c6fad7b892c7
Robin Schroer <sulami@peerwire.org>
2015-05-28 11:56:12 +0200

    svpino: always return an int, be it the position of a new list member

--- svpino/posofelem.hs
+++ svpino/posofelem.hs
@@ -3,13 +3,14 @@
 -- https://blog.svpino.com/2015/05/24/programming-challenge-the-position-of-the-element
 
 -- This is the obvious solution that won't scale well with large arrays, O(n).
-obvPos :: Int -> [Int] -> Maybe Int
+obvPos :: Int -> [Int] -> Int
 obvPos n l = find n l 0
   where
-    find :: Int -> [Int] -> Int -> Maybe Int
-    find n [] c = Nothing
-    find n l  c | n == head l = Just c
-                |   otherwise = find n (tail l) (c+1)
+    find :: Int -> [Int] -> Int -> Int
+    find n [x]    c |    n <= x = c
+                    | otherwise = c + 1
+    find n (x:xs) c |    n <= x = c
+                    | otherwise = find n xs (c+1)
 
 -- Other idea: Try splitting the list in halfs to reduce the number of
 -- comparisons down to O(log n).
