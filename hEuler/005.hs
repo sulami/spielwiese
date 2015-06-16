be4add334d3a0aeb6d831142060bb989dfc94273
Robin Schroer <sulami@peerwire.org>
2015-06-16 15:55:11 +0200

    hEuler: add 005

---
+++ hEuler/005.hs
@@ -0,0 +1,18 @@
+-- Smallest multiple
+-- Problem 5
+--
+-- 2520 is the smallest number that can be divided by each of the numbers from
+-- 1 to 10 without any remainder.
+--
+-- What is the smallest positive number that is evenly divisible by all of the
+-- numbers from 1 to 20?
+
+check :: Integer -> Bool
+check n = foldl (\r e -> if n `mod` e == 0 then r else False) True [1..20]
+
+work :: Integer -> Integer
+work n | check n   = n
+       | otherwise = work (n+1)
+
+main = print $ work 1
+
