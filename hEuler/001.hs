beaa22f77e6fb0d9b167de3a8f442e0ae2738d5b
Robin Schroer <sulami@peerwire.org>
2015-06-16 15:36:28 +0200

    add the first two euler solutions

---
+++ hEuler/001.hs
@@ -0,0 +1,15 @@
+-- Multiples of 3 and 5
+-- Problem 1
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+-- get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+fb :: Int -> Bool
+fb n | n `mod` 3 == 0 = True
+     | n `mod` 5 == 0 = True
+     |      otherwise = False
+
+main = print $ sum $ filter fb [1..999]
+
