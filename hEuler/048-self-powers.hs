e5d54393d829a7375a7ae75f2400b2c45238bf7d
Robin Schroer <sulami@peerwire.org>
2015-06-16 19:47:00 +0200

    hEuler: actually did not need digits in 48

--- hEuler/048-self-powers.hs
+++ hEuler/048-self-powers.hs
@@ -7,10 +7,6 @@
 
 {-# OPTIONS_GHC -O2 #-}
 
-digits :: Integral a => a -> [a]
-digits 0 = []
-digits n = digits (n `div` 10) ++ [n `mod` 10]
-
 selfPower :: Integral a => a -> a
 selfPower n = n^n
 
