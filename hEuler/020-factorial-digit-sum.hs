faf94b788e2f7f21bccbe8ec73e45120576d8a87
Robin Schroer <sulami@peerwire.org>
2015-06-16 18:57:14 +0200

    hEuler: add problem 20

---
+++ hEuler/020-factorial-digit-sum.hs
@@ -0,0 +1,22 @@
+-- Factorial digit sum
+-- Problem 20
+--
+-- n! means n × (n − 1) × ... × 3 × 2 × 1
+--
+-- For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+-- and the sum of the digits in the number 10! is
+-- 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+--
+-- Find the sum of the digits in the number 100!
+
+{-# OPTIONS_GHC -O2 #-}
+
+digits :: Integral a => a -> [a]
+digits 0 = []
+digits n = digits (n `div` 10) ++ [n `mod` 10]
+
+factorial :: Integral a => a -> a
+factorial n = foldl (\r e -> r * e) 1 [2..n]
+
+main = print $ sum $ digits $ factorial 100
+
