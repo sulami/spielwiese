791483f2da95452c7a9a404436a87daffabaf4dc
Robin Schroer <sulami@peerwire.org>
2015-06-18 12:25:15 +0200

    add a small programm to integrate functions in haskell

---
+++ stuff/riemann.hs
@@ -0,0 +1,10 @@
+-- Calculates the Riemann Sum, using a function term, the limits of the
+-- integration and the number of rectangles to use.
+
+riemannSum :: (Double -> Double) -> Double -> Double -> Double -> Double
+riemannSum f a b n = sum [f (a + i * dx) | i <- [0..n-1]] * dx
+  where
+    dx = (b - a) / n
+
+main = print $ riemannSum (\x -> x^2) 0 2 10000
+
