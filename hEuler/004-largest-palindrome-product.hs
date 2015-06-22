7decd29c42b8b8822bb00e51bf2393638764c9e3
Robin Schroer <sulami@peerwire.org>
2015-06-22 19:19:13 +0200

    hEuler: 004: fix it to actually get the largest product

--- hEuler/004-largest-palindrome-product.hs
+++ hEuler/004-largest-palindrome-product.hs
@@ -11,7 +11,7 @@
 palindromic :: Integer -> Bool
 palindromic n = show n == reverse (show n)
 
-main = print $ last [x*y | x <- [100..999],
-                           y <- [100..999],
-                           palindromic (x*y)]
+main = print $ maximum [x*y | x <- [100..999],
+                              y <- [100..999],
+                              palindromic (x*y)]
 
