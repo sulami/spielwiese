4ada88b666f7edf79bed12392074ff5314c39a8d
Robin Schroer <sulami@peerwire.org>
2015-06-22 18:52:16 +0200

    hEuler: add problem 3

---
+++ hEuler/003-largest-prime-factor.hs
@@ -0,0 +1,25 @@
+-- Largest prime factor
+-- Problem 3
+--
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+--
+-- What is the largest prime factor of the number 600851475143 ?
+
+{-# OPTIONS_GHC -O2 #-}
+
+divisors :: Integer -> [Integer]
+divisors n = [x | x <- [2..(n `div` 2)], n `mod` x == 0]
+
+pfd :: Integer -> [Integer]
+pfd n = pfd' n []
+  where
+    pfd' :: Integer -> [Integer] -> [Integer]
+    pfd' n r |        product r == n = r
+             | length (divs n r) > 0 = pfd' n (head (divs n r) : r)
+             |             otherwise = pfd' n (n `div` (product r) : r)
+      where
+        divs :: Integer -> [Integer] -> [Integer]
+        divs n r = divisors (n `div` (product r))
+
+main = print $ maximum $ pfd 600851475143
+
