fe25c99165707f3d1611e0d01da8b9d42239492d
Robin Schroer <sulami@peerwire.org>
2015-06-23 13:05:39 +0200

    hFuzzyFinder: handle scoring and sorting

--- hFuzzyFinder/fuzzy.hs
+++ hFuzzyFinder/fuzzy.hs
@@ -1,7 +1,11 @@
 -- This is a small and simple fuzzy filter that works by comparing a list of
 -- chars, the search input, against a list of list of chars, the possible
--- solutions. There are a couple of features still missing, like any form of
--- ranking or proper support for capital letters.
+-- solutions. If is case-insensitive and features scoring based on the length
+-- of the matched pattern and the position of the matched pattern in the
+-- result. There are no dependencies aside from the standard library. The usage
+-- looks like this:
+--
+-- fuzzyFinder "ae" ["Alphabet", "Aneurysm", "Albatross"]
 --
 -- The code is licensed under ISC and
 -- © Robin 'sulami' Schroer <sulami@peerwire.org>
@@ -9,25 +13,31 @@
 module Sulami.FuzzyFinder (fuzzyFinder) where
 
 import Data.Char
+import Data.List
 
 prepInput :: [[Char]] -> [([Char], [Char])]
 prepInput i = zip i $ map (map toLower) i
 
-partOf :: Char -> [Char] -> (Bool, [Char])
-partOf _ []     = (False, [])
-partOf c (x:xs) |    c == x = (True, xs)
-                | otherwise = partOf c xs
+partOf :: Char -> [Char] -> Int -> (Bool, Int)
+partOf _ []     r = (False, 0)
+partOf c (x:xs) r |    c == x = (True, r + 1)
+                  | otherwise = partOf c xs $ r + 1
 
-match :: [Char] -> ([Char], [Char]) -> Bool
-match i s = match' i (snd s)
+match :: [Char] -> ([Char], [Char]) -> (Bool, [Int])
+match i s = match' i (snd s) []
   where
-    match' :: [Char] -> [Char] -> Bool
-    match' []     _ = True
-    match' (x:xs) s | fst check = match' xs (snd check)
-                    | otherwise = False
+    match' :: [Char] -> [Char] -> [Int] -> (Bool, [Int])
+    match' []     _ r = (True, r)
+    match' (x:xs) s r | fst check = match' xs (drop (snd check) s)
+                                           $ r ++ [snd check]
+                      | otherwise = (False, r)
       where
-        check = partOf x s
+        check = partOf x s 0
 
 fuzzyFinder :: [Char] -> [[Char]] -> [[Char]]
-fuzzyFinder i l = map fst $ filter (match i) $ prepInput l
+fuzzyFinder i l = map fst . map snd $ c
+  where
+    c = sort $ zip (zip ((map sum . map tail) s) (map head s)) p
+    s = map snd $ map (match i) p
+    p = filter (fst . match i) $ prepInput l
 
