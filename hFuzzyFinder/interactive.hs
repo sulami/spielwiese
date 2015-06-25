7144e42753bdd1e7db2212a05365297173b27ca0
Robin Schroer <sulami@peerwire.org>
2015-06-25 13:26:00 +0200

    hFuzzyFinder: loads of cosmetic changes for the interactive demo

--- hFuzzyFinder/interactive.hs
+++ hFuzzyFinder/interactive.hs
@@ -1,13 +1,23 @@
-import System.Environment
+import System.Environment (getArgs)
+import System.IO (hFlush, stdout)
 
 import FuzzyFinder
 
 readL :: String -> [String]
 readL a = read $ "[" ++ a ++ "]"
 
+putL :: [String] -> IO ()
+putL [x]    = putStrLn x
+putL (x:xs) = do putStrLn x
+                 putL xs
+
 main = do args <- getArgs
-          if length args == 2 then do
-            list <- readFile $ args !! 1
-            print $ fuzzyFinder (args !! 0) $ readL list
+          if length args == 1 then do
+            list <- fmap readL . readFile $ head args
+            putStr "Entery query: "
+            hFlush stdout
+            query <- getLine
+            putL $ fuzzyFinder query list
           else
-            print "Wrong number of args"
+            putStrLn "Wrong number of args"
+
