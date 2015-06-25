087a5c785d292e7abaca876db4cd0a63ba25e4d9
Robin Schroer <sulami@peerwire.org>
2015-06-25 18:58:32 +0200

    hEuler: add problem 15

---
+++ hEuler/015-lattice-paths.hs
@@ -0,0 +1,22 @@
+-- Lattice paths
+-- Problem 15
+--
+-- Starting in the top left corner of a 2×2 grid, and only being able to move
+-- to the right and down, there are exactly 6 routes to the bottom right
+-- corner.
+--
+-- How many such routes are there through a 20×20 grid?
+
+{-# OPTIONS_GHC -O2 #-}
+
+paths :: Int -> Int
+paths s = paths' s 0 0 0
+  where
+    paths' :: Int -> Int -> Int -> Int -> Int
+    paths' s r x y | x < s && y < s = (paths' s r (x+1) y) + (paths' s r x (y+1))
+                   |          x < s = paths' s r (x+1) y
+                   |          y < s = paths' s r x (y+1)
+                   |      otherwise = r+1
+
+main = print $ paths 20
+
