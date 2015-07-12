215ec340a4bb4eadfdf3e0b33981baf4a85ab662
Robin Schroer <sulami@peerwire.org>
2015-07-12 21:27:38 +0200

    add a first part of Sim.hs

---
+++ sim/Sim.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+import Data.List (sortBy)
+import Data.Maybe (fromJust)
+import System.Random (randomRIO)
+
+type Property   = (String, IO Float)
+type Ent        = [Property]
+type Generation = [Ent]
+
+data Config     = Config {
+    genSize         :: Int,
+    mutProbability  :: Float,
+    mutRange        :: Float
+  }
+
+mutate :: Config -> Ent -> [Ent]
+mutate conf base = (base : mutate conf (mut base))
+  where
+    mut :: Ent -> Ent
+    mut base = map chProp base
+
+    chProp :: Property -> Property
+    chProp (n, p) = let np = do r1 <- randomRIO (0, 1) :: IO Float
+                                let v = if r1 <= mutProbability conf
+                                        then randomRIO (0, mutRange conf)
+                                        else return 0
+                                nv <- v
+                                op <- p
+                                return (op * (1 + nv))
+                    in (n, np)
+
+evolution :: Config -> Ent -> Generation
+evolution conf base = take (genSize conf) $ mutate conf base
+
+test :: Ord a => (Ent -> a) -> Generation -> Ent
+test f gen = fst $ last $ sortBy s $ zip gen $ map f gen
+  where
+    s :: (Ord a) => (b, a) -> (b, a) -> Ordering
+    s (_, a) (_, b) | a > b     = GT
+                    | a < b     = LT
+                    | otherwise = EQ
+
+main = do let conf = Config 5 0.5 0.1
+          let base = [("Height", return 100)]
+          let score = (\e -> 100 - (fromJust $ lookup "Height" e))
+          let gen0 = evolution conf base
+          print $ "fds"
+
