e1193ff9bfa9a113cb3d72105240edc221c4cacc
Robin Schroer <sulami@peerwire.org>
2015-07-21 01:46:59 +0200

    more reformatting, more speedups

--- astar/run.hs
+++ astar/run.hs
@@ -19,50 +19,35 @@ possibleWays g (x,y) = [(x1,y1) | x1 <- [0..(x+1)], y1 <- [0..(y+1)],
                                   g !! y1 !! x1 == 'F' || g !! y1 !! x1 == '-'
                                   ]
 
-parseDir :: Coord -> Coord -> String
-parseDir (x0,y0) (x1,y1) | x0 > x1 = "W"
-                         | x0 < x1 = "E"
-                         | y0 > y1 = "N"
-                         | y0 < y1 = "S"
-
 parsePath :: Path -> String
 parsePath p = concat $ map (\(a,b) -> parseDir a b) $ zip (init p) (tail p)
+  where
+    parseDir :: Coord -> Coord -> String
+    parseDir (x0,y0) (x1,y1) | x0 > x1 = "W"
+                             | x0 < x1 = "E"
+                             | y0 > y1 = "N"
+                             | y0 < y1 = "S"
 
 flood :: Grid -> Coord -> [Path]
-flood grid pos = fl grid [[pos]] [pos]
+flood grid pos = fl grid [[pos]]
   where
-    fl :: Grid -> [Path] -> [Coord] -> [Path]
-    fl _ _ [] = []
-    fl grid path pos
-      | finished grid (last pos) = path
-      | otherwise = let nexts = map (pssbl grid (last path)) pos
-                    in path ++ concat (map (\n -> fl grid [last path ++ n] n) nexts)
-    pssbl :: Grid -> Path -> Coord -> Path
-    pssbl grid path pos = [(x,y) | (x,y) <- possibleWays grid pos,
-                                   not $ (x,y) `elem` path]
+    fl :: Grid -> [Path] -> [Path]
+    fl grid paths = paths ++ fl grid (concat (map (addRoutes grid) paths))
 
-finished :: Grid -> Coord -> Bool
-finished g (x,y) = g !! y !! x == 'F'
+    addRoutes :: Grid -> Path -> [Path]
+    addRoutes grid path = [ path ++ [p] | p <- possibleWays grid $ last path,
+                                          not $ p `elem` path ]
 
-cut :: Grid -> Path -> Path
-cut _ [] = []
-cut g p = takeWhile (not . finished g) p ++ (take 1 (dropWhile (not . finished g) p))
-
-shortest :: [Path] -> Path
-shortest ps = snd $ minimum $ zip (map length ps) ps
+reachesTarget :: Grid -> Path -> Bool
+reachesTarget g p = let (x,y) = last p in g !! y !! x == 'F'
 
 printPath :: String -> IO ()
 printPath []     = return ()
 printPath (x:xs) = do putStr $ x : "\n"
                       printPath xs
 
-reachesTarget :: Grid -> Path -> Bool
-reachesTarget g p = let (x,y) = last p in g !! y !! x == 'F'
-
 main = do grid <- fmap lines getContents
           let start = findStart grid 0
-          let paths = filter (reachesTarget grid)
-                      $ map (cut grid) (flood grid start)
-          let short = head paths
-          printPath $ parsePath short
+          let path = head $ filter (reachesTarget grid) $ flood grid start
+          printPath $ parsePath path
 
