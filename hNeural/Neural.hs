efd4ed558294265c44a7b2504cca37f721968b37
Robin Schroer <sulami@peerwire.org>
2015-07-20 12:15:19 +0200

    hNeural: use doubles for everything

--- hNeural/Neural.hs
+++ hNeural/Neural.hs
@@ -6,8 +6,8 @@ module Neural (
 
 import Data.Matrix
 
-type Data    a = Matrix a
-type Synapse a = Matrix a
+type Data          = Matrix Double
+type Synapse       = Matrix Double
 
 dot :: Matrix Double -> Matrix Double -> Matrix Double
 dot a b = fromLists [zipWith (*) (toList a) (toList b)]
@@ -19,18 +19,18 @@ sigmoidDeriv :: Double -> Double
 sigmoidDeriv x = x * (1 - x)
 
 -- | Use the synapse to try to predict the result of the data.
-predict :: Synapse Double -> Data Double -> Data Double
+predict :: Synapse -> Data -> Data
 predict syn d1 = fmap sigmoid $ multStd2 d1 syn
 
-delta :: Data Double -> Data Double -> Data Double
+delta :: Data -> Data -> Data
 delta want res = transpose $ (want - res) `dot` (fmap sigmoidDeriv res)
 
-updWght :: Synapse Double -> Data Double -> Data Double -> Synapse Double
+updWght :: Synapse -> Data -> Data -> Synapse
 updWght syn d dlt = let mod = (transpose d) * dlt in syn + mod
 
 -- | Use a base synapse, input data and expected output to train the synapse
 -- for c iterations.
-train :: Synapse Double -> Data Double -> Data Double -> Int -> Synapse Double
+train :: Synapse -> Data -> Data -> Int -> Synapse
 train syn _  _  0 = syn
 train syn d1 d2 c = let dlt = delta d2 $ predict syn d1
                         ns = updWght syn d1 dlt
