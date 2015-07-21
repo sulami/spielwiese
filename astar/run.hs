d867cd6ba3c6d6a6119cad0816ebfe9ee91fb996
Robin Schroer <sulami@peerwire.org>
2015-07-21 18:06:21 +0200

    use a* instead of a gready flood fill

--- astar/run.hs
+++ astar/run.hs
@@ -6,13 +6,13 @@ type Grid = [String]
 type Coord = (Int, Int)
 type Path = [Coord]
 
-findStart :: Grid -> Int -> Coord
-findStart (x:xs) n | 'S' `elem` x = (fs x 0, n)
-                   | otherwise    = findStart xs $ n+1
+find :: Grid -> Char -> Int -> Coord
+find (x:xs) c n | c `elem` x = (f' x c 0, n)
+                | otherwise  = find xs c $ n+1
   where
-    fs :: String -> Int -> Int
-    fs (x:xs) n | 'S' == x  = n
-                | otherwise = fs xs $ n+1
+    f' :: String -> Char -> Int -> Int
+    f' (x:xs) c n | c == x    = n
+                  | otherwise = f' xs c $ n+1
 
 
 parsePath :: Path -> String
@@ -24,11 +24,16 @@ parsePath p = concat $ map (\(a,b) -> parseDir a b) $ zip (init p) (tail p)
                              | y0 > y1 = "N"
                              | y0 < y1 = "S"
 
-flood :: Grid -> Coord -> [Path]
-flood grid pos = fl grid [[pos]]
+flood :: Grid -> Coord -> Coord -> [Path]
+flood grid fin pos = fl grid fin [[pos]]
   where
-    fl :: Grid -> [Path] -> [Path]
-    fl grid paths = paths ++ fl grid (concat (map (addRoutes grid paths) paths))
+    fl :: Grid -> Coord -> [Path] -> [Path]
+    fl grid fin paths
+      | any (\p -> last p == fin) paths = filter (\p -> last p == fin) paths
+      | otherwise = let costs = zip (map (cost fin) paths) paths
+                        best = snd $ minimum costs
+                        pb = addRoutes grid paths best
+                    in fl grid fin $ filter (/= best) paths ++ pb
 
     addRoutes :: Grid -> [Path] -> Path -> [Path]
     addRoutes grid ps path = [ path ++ [p] | p <- possibleWays grid ps $ last path ]
@@ -45,8 +50,11 @@ flood grid pos = fl grid [[pos]]
                                          not $ (x1,y1) `elem` (nub (concat ps))
                                          ]
 
-reachesTarget :: Grid -> Path -> Bool
-reachesTarget g p = let (x,y) = last p in g !! y !! x == 'F'
+    cost :: Coord -> Path -> Int
+    cost fin path = (length path - 1) + (dist (last path) fin)
+
+    dist :: Coord -> Coord -> Int
+    dist (x0,y0) (x1,y1) = abs (x0-x1) + abs (y0-y1)
 
 printPath :: String -> IO ()
 printPath []     = return ()
@@ -54,7 +62,8 @@ printPath (x:xs) = do putStr $ x : "\n"
                       printPath xs
 
 main = do grid <- fmap lines getContents
-          let start = findStart grid 0
-          let path = head $ filter (reachesTarget grid) $ flood grid start
+          let start = find grid 'S' 0
+          let fin = find grid 'F' 0
+          let path = head $ flood grid fin start
           printPath $ parsePath path
 
