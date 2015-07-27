cc5ead7084e487c2cbbdd1b2f25bdbbd715719bb
Robin Schroer <sulami@peerwire.org>
2015-07-27 18:18:53 +0200

    pass the cost and possible route functions into flood

--- astar/lib/Astar.hs
+++ astar/lib/Astar.hs
@@ -1,5 +1,6 @@
 module Astar (
   Grid, Coord, Path,
+  PossibleWaysFun, CostFun,
   flood
   ) where
 
@@ -11,36 +12,20 @@ type Grid = [String]
 type Coord = (Int, Int)
 type Path = [Coord]
 
-flood :: Grid -> Coord -> Coord -> [Path]
-flood grid fin pos = fl grid fin [[pos]]
+type PossibleWaysFun = Grid -> [Path] -> Coord -> Path
+type CostFun = Coord -> Path -> Int
+
+flood :: Grid -> Coord -> Coord -> PossibleWaysFun -> CostFun -> [Path]
+flood grid fin pos pwf cf = fl grid fin pwf cf [[pos]]
   where
-    fl :: Grid -> Coord -> [Path] -> [Path]
-    fl grid fin paths
+    fl :: Grid -> Coord -> PossibleWaysFun -> CostFun -> [Path] -> [Path]
+    fl grid fin pwf cf paths
       | any (\p -> last p == fin) paths = filter (\p -> last p == fin) paths
       | otherwise = let best = snd $ minimum
-                               $ zip (parMap rpar (cost fin) paths) paths
-                        pb = addRoutes grid paths best
-                    in fl grid fin $ filter (/= best) paths ++ pb
+                               $ zip (parMap rpar (cf fin) paths) paths
+                        pb = addRoutes grid paths best pwf
+                    in fl grid fin pwf cf $ filter (/= best) paths ++ pb
 
-    addRoutes :: Grid -> [Path] -> Path -> [Path]
-    addRoutes grid ps path = [path ++ [p] | p <- possibleWays grid ps
-                                                 $ last path]
-
-    possibleWays :: Grid -> [Path] -> Coord -> Path
-    possibleWays g ps (x,y) = [(x1,y1) | y1 <- [(y-1)..(y+1)],
-                                         y1 >= 0,
-                                         y1 < length g,
-                                         x1 <- [(x-1)..(x+1)],
-                                         x1 >= 0,
-                                         x1 < length (g !! y1),
-                                         x-x1 == 0 || y-y1 == 0,
-                                         g !! y1 !! x1 /= 'X',
-                                         not $ (x1,y1) `elem` (concat ps)
-                                         ]
-
-    cost :: Coord -> Path -> Int
-    cost fin path = let l = last path in (length path - 1) + (dist l fin)
-
-    dist :: Coord -> Coord -> Int
-    dist (x0,y0) (x1,y1) = abs (x0-x1) + abs (y0-y1)
+    addRoutes :: Grid -> [Path] -> Path -> PossibleWaysFun -> [Path]
+    addRoutes grid ps path pwf = [path ++ [p] | p <- pwf grid ps $ last path]
 
--- astar/src/Main.hs
+++ astar/src/Main.hs
@@ -2,6 +2,23 @@ module Main where
 
 import Astar
 
+possibleWays :: PossibleWaysFun
+possibleWays g ps (x,y) = [ (x1,y1) | y1 <- [(y-1)..(y+1)],
+                                      y1 >= 0,
+                                      y1 < length g,
+                                      x1 <- [(x-1)..(x+1)],
+                                      x1 >= 0,
+                                      x1 < length (g !! y1),
+                                      x-x1 == 0 || y-y1 == 0,
+                                      g !! y1 !! x1 /= 'X',
+                                      not $ (x1,y1) `elem` (concat ps) ]
+
+cost :: CostFun
+cost fin path = let l = last path in (length path - 1) + (dist l fin)
+  where
+    dist :: Coord -> Coord -> Int
+    dist (x0,y0) (x1,y1) = abs (x0-x1) + abs (y0-y1)
+
 find :: Grid -> Char -> Int -> Coord
 find (x:xs) c n | c `elem` x = (f' x c 0, n)
                 | otherwise  = find xs c $ n+1
@@ -22,6 +39,6 @@ replace o x y c = let (rpre,rpost) = splitAt y o
 main = do grid <- fmap lines getContents
           let start = find grid 'S' 0
           let fin = find grid 'F' 0
-          let path = head $ flood grid fin start
+          let path = head $ flood grid fin start possibleWays cost
           mapM_ putStrLn $ drawPath grid path
 
