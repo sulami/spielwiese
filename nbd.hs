bdd8cd1a50949416415c53d4fb1e4b2c072e8524
Robin Schroer <sulami@peerwire.org>
2015-07-27 02:49:55 +0200

    add eval and parser for isZero

--- nbd.hs
+++ nbd.hs
@@ -9,12 +9,14 @@ type PState = [(String, Integer)]
 
 data Expr = NbdInt Integer
           | NbdSymbol String
+          | NbdIsZero Expr [Expr]
           | NbdNothing
 
 instance Show Expr where
+  show NbdNothing = "<Nothing>"
   show (NbdInt x) = show x
   show (NbdSymbol x) = x
-  show NbdNothing = ""
+  show (NbdIsZero x _) = "<isZero " ++ (show x) ++ ">"
 
 type NbdResult a = Control.Monad.State.State PState a
 
@@ -24,6 +26,12 @@ eval (NbdInt x) = return $ NbdInt x
 eval (NbdSymbol x) = do vars <- get
                         let val = lookup x vars
                         return $ NbdInt $ fromJust val
+eval (NbdIsZero x e) = do y <- nbdIsZero x
+                          if y
+                            then do s0 <- get
+                                    inIf <- run s0 e
+                                    return inIf
+                            else return NbdNothing
 
 run :: PState -> [Expr] -> NbdResult Expr
 run s0 []     = return NbdNothing
@@ -49,6 +57,23 @@ parseSymbol :: Parser Expr
 parseSymbol = do f <- letter
                  r <- many (letter <|> digit)
                  return $ NbdSymbol $ f : r
+
+parseValue :: Parser Expr
+parseValue = (try parseInteger) <|> (try parseSymbol)
+
+parseIsZero :: Parser Expr
+parseIsZero = do char '?' ; skipMany space
+                 x <- parseValue `sepEndBy` (many1 space)
+                 char '{'
+                 inner <- parseExpr
+                 char '}'
+                 return inner
+
+parseExpr :: Parser Expr
+parseExpr = do skipMany space
+               x <- parseExpr
+               skipMany space ; eof
+               return x
 
 main = do input <- fmap words getContents
           mapM_ print input
