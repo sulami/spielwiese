acca0d25cf1171a055d37881139d7867e4d9962c
Robin Schroer <sulami@peerwire.org>
2015-08-04 23:59:18 +0200

    render some points on a circle

--- hgl/src/Main.hs
+++ hgl/src/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import Graphics.UI.GLUT
 
+myPoints :: [(GLfloat, GLfloat, GLfloat)]
+myPoints = [ (sin (2*pi*k/100), cos (2*pi*k/100), 0) | k <- [1..100] ]
+
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
+  renderPrimitive Points $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) myPoints
   flush
 
 main = do
