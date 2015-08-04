f22a26573832f9e966fad8e8d3dcd66f01ec92c6
Robin Schroer <sulami@peerwire.org>
2015-08-04 23:50:28 +0200

    spawn a window, yay

--- hgl/src/Main.hs
+++ hgl/src/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
-main = putStrLn "Hello, World!"
+import Graphics.UI.GLUT
+
+display :: DisplayCallback
+display = do
+  clear [ ColorBuffer ]
+  flush
+
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "Hello, World!"
+  displayCallback $= display
+  mainLoop
 
