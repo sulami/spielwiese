342b8a6de1f3ee29fc57a7139111a4f55a7475dc
Robin Schroer <sulami@peerwire.org>
2015-08-04 16:41:37 +0200

    do not show the not yet guessed words

--- texttwist/Main.hs
+++ texttwist/Main.hs
@@ -1,17 +1,28 @@
 module Main where
 
-import Data.List (nub, permutations, subsequences)
+import Data.Ord (comparing)
+import Data.List (nub, permutations, sort, sortBy, subsequences)
 import System.Random (randomRIO)
 
+type GameState = [(String, Bool)]
+
 subwords :: [String] -> String -> [String]
-subwords list base = nub $ filter (`elem` list) $ concat
-                     $ filter (\w -> 2 < length w) $ map permutations
+subwords list base = sortBy (comparing length) $ nub $ filter (`elem` list)
+                     $ concat $ filter (\w -> 2 < length w) $ map permutations
                      $ subsequences base
+
+printState :: GameState -> IO ()
+printState s0 = mapM_ putStrLn $ map filtrate s0
+  where
+    filtrate (w,s) | s         = w
+                   | otherwise = "[" ++ show (length w) ++ " letters]"
 
 main = do wordlist <- fmap lines $ readFile "words"
           let initlist = filter (\w -> length w > 3 && length w < 7) wordlist
           initn <- randomRIO (0, length initlist - 1)
           let init = initlist !! initn
-          putStrLn $ "Init: " ++ init
-          mapM_ putStrLn $ subwords wordlist init
+          let words = subwords wordlist init
+          let s0 = zip words $ repeat False
+          putStrLn $ "Letters: " ++ sort init
+          printState s0
 
