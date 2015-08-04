0818d1a016c415d298a7fb9e0a0cde1acf5b54b8
Robin Schroer <sulami@peerwire.org>
2015-08-05 01:23:38 +0200

    small rotating circle

--- hgl/lib/Engine/Display.hs
+++ hgl/lib/Engine/Display.hs
@@ -7,13 +7,15 @@ import Data.IORef
 import Graphics.UI.GLUT
 
 myPoints :: [(GLfloat, GLfloat, GLfloat)]
-myPoints = [ (sin (2*pi*k/100), cos (2*pi*k/100), 0) | k <- [1..100] ]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
 
 display :: IORef GLfloat -> DisplayCallback
 display angle = do
   clear [ ColorBuffer ]
   loadIdentity
   a <- get angle
+  rotate a $ Vector3 0 0 1
+  scale 0.2 0.2 (0.2 :: GLfloat)
   renderPrimitive Points $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) myPoints
   swapBuffers
 
@@ -24,6 +26,6 @@ reshape size = do
 
 idle :: IORef GLfloat -> IdleCallback
 idle angle = do
-  angle $~! (+ 0.1)
+  angle $~! (+ 1)
   postRedisplay Nothing
 
