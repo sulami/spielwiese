1334bcac04100ade2995798a0a4da63916bd9322
Robin Schroer <sulami@peerwire.org>
2015-08-06 10:24:23 +0200

    add a score variable to the gamestate type

--- texttwist/Main.hs
+++ texttwist/Main.hs
@@ -6,7 +6,7 @@ import System.Console.ANSI (clearScreen, setCursorPosition)
 import System.IO (hFlush, stdout)
 import System.Random (randomRIO)
 
-type GameState = [(String, Bool)]
+data GameState = GameState [(String, Bool)] Integer
 
 subwords :: [String] -> String -> [String]
 subwords list base = sortBy (comparing length) $ sort $ nub
@@ -15,30 +15,31 @@ subwords list base = sortBy (comparing length) $ sort $ nub
                      $ subsequences base
 
 guess :: GameState -> String -> GameState
-guess s0 w0 | w0 `elem` (map fst s0) = map (\(w,s) -> (w,s || w0 == w)) s0
-            | otherwise              = s0
+guess (GameState g0 s0) w0
+  | w0 `elem` (map fst g0) = GameState (map (\(w,s) -> (w,s || w0 == w)) g0) s0
+  | otherwise              = GameState g0 s0
 
 printState :: GameState -> IO ()
-printState s0 = putStrLn $ unwords $ map filtrate s0
+printState (GameState g0 s0) = putStrLn $ unwords $ map filtrate g0
   where
     filtrate (w,s) | s         = "\x1b[32m" ++ w ++ "\x1b[39m"
                    | otherwise = [ '_' | _ <- [1..(length w)] ]
 
 mainLoop :: String -> GameState -> IO ()
-mainLoop w0 s0 = if all snd s0
-                   then putStrLn "Fin!"
-                   else do clearScreen
-                           setCursorPosition 0 0
-                           printState s0
-                           putStr prompt
-                           hFlush stdout
-                           g <- getLine
-                           let s1 = guess s0 g
-                           mainLoop w0 s1
+mainLoop w0 gs0@(GameState g0 s0) = if all snd g0
+                                      then putStrLn "Fin!"
+                                      else do clearScreen
+                                              setCursorPosition 0 0
+                                              printState gs0
+                                              putStr prompt
+                                              hFlush stdout
+                                              g <- getLine
+                                              let g1 = guess gs0 g
+                                              mainLoop w0 g1
   where
     prompt :: String
-    prompt = let d = show $ length $ filter snd s0
-              in "[" ++ d ++ "/" ++ show (length s0) ++ "] " ++ w0 ++ " > "
+    prompt = let d = show $ length $ filter snd g0
+              in "[" ++ d ++ "/" ++ show (length g0) ++ "] " ++ w0 ++ " > "
 
 main = do wordlist <- fmap lines $ readFile "words"
           let initlist = filter (\w -> length w == 6) wordlist
@@ -46,5 +47,5 @@ main = do wordlist <- fmap lines $ readFile "words"
           let w0 = initlist !! initn
           let words = subwords wordlist w0
           let s0 = zip words $ repeat False
-          mainLoop (sort w0) s0
+          mainLoop (sort w0) $ GameState s0 0
 
