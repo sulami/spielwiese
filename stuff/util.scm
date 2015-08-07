a044c08a6866220cdb3f8dce41d494ed2b713c87
Robin Schroer <sulami@peerwire.org>
2015-08-07 20:47:53 +0200

    remove the extra lambdas in function defs

--- stuff/util.scm
+++ stuff/util.scm
@@ -1,17 +1,16 @@
 ; Factorial (n!).
-(define fact
-        (lambda (n)
-          (if (eq? 1 n)
+(define (fact n)
+         (if (eq? 1 n)
             n
-            (* n (fact (- n 1))))))
+            (* n (fact (- n 1)))))
 
 ; Construct a list from s to e.
 (define (cl s e)
         (cls s 1 e))
 
 ; Construct a list from s to e defining the step size.
-(define cls
-        (lambda (s ss e)
-          (if (> s e)
+(define (cls s ss e)
+         (if (> s e)
             '()
-            (cons s (cls (+ s ss) ss e)))))
+            (cons s (cls (+ s ss) ss e))))
+
