0c43f7cfbbde21414785f415cb8d7ef796dd6f1a
Robin Schroer <sulami@peerwire.org>
2015-08-07 20:53:29 +0200

    more list creation stuff

--- stuff/util.scm
+++ stuff/util.scm
@@ -4,13 +4,17 @@
             n
             (* n (fact (- n 1)))))
 
-; Construct a list from s to e.
-(define (cl s e)
-        (cls s 1 e))
-
 ; Construct a list from s to e defining the step size.
 (define (cls s ss e)
          (if (> s e)
             '()
             (cons s (cls (+ s ss) ss e))))
+
+; Construct a list from s to e.
+(define (cl s e)
+        (cls s 1 e))
+
+; Construct a list from 1 to e with step size 1.
+(define (oneto e)
+        (cl 1 e))
 
