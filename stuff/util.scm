92f8406f6db8a8bd9e73c0590f8dddbd9d0a4491
Robin Schroer <sulami@peerwire.org>
2015-08-07 20:47:05 +0200

    define cl in terms of cls

--- stuff/util.scm
+++ stuff/util.scm
@@ -6,11 +6,8 @@
             (* n (fact (- n 1))))))
 
 ; Construct a list from s to e.
-(define cl
-        (lambda (s e)
-          (if (> s e)
-            '()
-            (cons s (cl (+ s 1) e)))))
+(define (cl s e)
+        (cls s 1 e))
 
 ; Construct a list from s to e defining the step size.
 (define cls
