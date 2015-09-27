2fac092cb19163d36c3e2faa8cbf6a3d88082949
Robin Schroer <sulami@peerwire.org>
2015-09-27 15:58:57 +0200

    codec: work on chunking frames

--- hCodec/Video.hs
+++ hCodec/Video.hs
@@ -4,9 +4,26 @@
 module Video (
 ) where
 
+import           Data.List (foldl')
+
+-- | The datatype used for single colour channels. Possibly subject to changes.
+-- Colours are expected to be between 0 and 255.
+type Channel = Int
+
 -- | A simple RGB colour.
-newtype Colour = Colour (Int, Int, Int)
+newtype Colour = Colour (Channel, Channel, Channel)
   deriving (Eq, Show)
+
+-- | Average colours.
+avgColour :: [Colour] -> Colour
+avgColour [] = error "Empty list"
+avgColour cs = r (length cs) $ foldr1 a cs
+  where
+    a :: Colour -> Colour -> Colour
+    a (Colour (r0,g0,b0)) (Colour (r1,g1,b1)) = Colour (r0+r1,g0+g1,b0+b1)
+
+    r :: Int -> Colour -> Colour
+    r l (Colour (rc,gc,bc)) = Colour (rc `div` l, gc `div` l, bc `div` l)
 
 type Pixel = Colour
 
@@ -15,7 +32,7 @@ data Chunk = Chunk
   { chunkWidth  :: Int
   , chunkHeight :: Int
   , chunkColour :: Colour
-  }
+  } deriving (Show)
 
 -- | A single frame, composed of Pixels.
 data PixelFrame = PixelFrame
@@ -48,7 +65,19 @@ frameToChunks :: (Int, Int) -> PixelFrame -> ChunkFrame
 frameToChunks (cw,ch) (PixelFrame w h ps) = ChunkFrame w h $ toChunks ps
   where
     toChunks :: [Pixel] -> [Chunk]
-    toChunks = undefined
+    toChunks [] = []
+    toChunks ps = let (top,rest) = splitAt (w * ch) ps
+                      crow = map (Chunk cw ch . avgColour) . cols $ toLines top
+                  in crow ++ toChunks rest
+      where
+        cols :: [[Pixel]] -> [[Pixel]]
+        cols []     = []
+        cols (c:cs) = concatMap (take cw) cs : cols cs
+
+    toLines :: [Pixel] -> [[Pixel]]
+    toLines [] = []
+    toLines ps = let (a,b) = splitAt w ps
+                  in a : toLines b
 
 -- | Convert a ChunkFrame to a PixelFrame.
 frameToPixels :: ChunkFrame -> PixelFrame
