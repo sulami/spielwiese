fb417073c0531441c1d9faad73c52767c5778919
Robin Schroer <sulami@peerwire.org>
2015-12-09 01:18:42 +0100

    advent: day 8 part 2

--- advent/08.hs
+++ advent/08.hs
@@ -6,9 +6,6 @@ alphaNum = ['0'..'9'] ++ ['a'..'z']
 validTokens :: String
 validTokens = alphaNum ++ ['\\','"']
 
-codeLength :: String -> Int
-codeLength = length . filter (`elem` validTokens)
-
 memoryLength :: String -> Int
 memoryLength [] = 0
 memoryLength (x:xs)
@@ -18,9 +15,19 @@ memoryLength (x:xs)
                           then 1 + memoryLength (drop 1 xs)
                           else 1 + memoryLength (drop 3 xs)
 
+encodedLength :: String -> Int
+encodedLength [] = 0
+encodedLength (x:xs)
+  | x `elem` alphaNum           = 1 + encodedLength xs
+  | x == '\\'                   = 2 + encodedLength xs
+  | x == '"'                    = 2 + encodedLength xs
+
 main = do
   indata <- readFile "08.input"
-  let cl = codeLength indata
-      ml = memoryLength $ filter (`elem` validTokens) indata
+  let pd = filter (`elem` validTokens) indata
+      cl = length pd
+      ml = memoryLength pd
+      nl = length $ lines indata
   print $ cl - ml
+  print $ (nl * 2 + encodedLength pd) - cl
 
