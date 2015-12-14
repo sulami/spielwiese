3d973ce9bcc12750d03c847b37231ff5938572e9
Robin Schroer <sulami@peerwire.org>
2015-12-14 15:49:34 +0100

    advent: day 14 part 2

--- advent/14.hs
+++ advent/14.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.List (maximumBy)
+import Data.List (maximumBy, nub)
 import Data.Ord (comparing)
 
 type Reindeer = (String, (Int, Int, Int))
@@ -24,7 +24,20 @@ travelledAt t r@(name, (v,vt,pt))
   | t > 0     = v * min t vt + travelledAt (t - vt - pt) r
   | otherwise = 0
 
+speeds :: [Reindeer] -> [[(String, Int)]]
+speeds rs = [ [ (fst &&& travelledAt t) r | r <- rs ] | t <- [1..2503] ]
+
+leaders :: [(String, Int)] -> [String]
+leaders rs = let best = maximum $ map snd rs
+              in map fst $ filter (\(_,d) -> d == best) rs
+
+points :: [String] -> [(String, Int)]
+points rs = let names = nub rs
+            in map (id &&& \n -> length $ filter (== n) rs) names
+
 main = do
-  let speeds = map (fst &&& travelledAt 2503) reindeers
-  print $ maximumBy (comparing snd) speeds
+  let dists = map (fst &&& travelledAt 2503) reindeers
+      winner = maximumBy (comparing snd)
+  print $ winner dists
+  print . winner . points . concatMap leaders $ speeds reindeers
 
