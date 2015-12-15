db01d7b286f10e88fd214fdc3b10ce32f51d62c5
Robin Schroer <sulami@peerwire.org>
2015-12-15 09:32:37 +0100

    advent: day 15 part 2

--- advent/15.hs
+++ advent/15.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Control.Arrow ((&&&))
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
 type Ingridient = (Int, Int, Int, Int, Int)
 
 propSums :: [(Int, Ingridient)] -> [Int]
@@ -10,10 +14,16 @@ propSums ((n,(cap,dur,fla,tex,cal)):is) =
 score :: [(Int, Ingridient)] -> Int
 score is = product . map (max 0) $ propSums is
 
+cals :: [(Int, Ingridient)] -> Int
+cals is = sum $ map (\(n,(_,_,_,_,cal)) -> cal*n) is
+
 main = do
   let is = [(4,-2,0,0,5), (0,5,-1,0,8), (-1,0,5,0,6), (0,0,-2,2,1)]
-  print . maximum $ map score [ zip [a,b,c,100-a-b-c] is | a <- [1..97],
-                                                           b <- [1..97],
-                                                           c <- [1..97],
-                                                           a+b+c <= 99 ]
+      ns = [ zip [a,b,c,100-a-b-c] is | a <- [1..97],
+                                        b <- [1..97],
+                                        c <- [1..97],
+                                        a+b+c <= 99 ]
+  print . maximum $ map score ns
+  print . snd . maximumBy (comparing snd) . filter ((== 500) . fst) $
+    map (cals &&& score) ns
 
