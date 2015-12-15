91d9a22dfb397a0bd6e18b230fa9458fb5161904
Robin Schroer <sulami@peerwire.org>
2015-12-15 09:25:07 +0100

    advent: day 15 part 1

---
+++ advent/15.hs
@@ -0,0 +1,19 @@
+module Main where
+
+type Ingridient = (Int, Int, Int, Int, Int)
+
+propSums :: [(Int, Ingridient)] -> [Int]
+propSums [] = repeat 0
+propSums ((n,(cap,dur,fla,tex,cal)):is) =
+  zipWith (+) (propSums is) $ map (*n) [cap,dur,fla,tex]
+
+score :: [(Int, Ingridient)] -> Int
+score is = product . map (max 0) $ propSums is
+
+main = do
+  let is = [(4,-2,0,0,5), (0,5,-1,0,8), (-1,0,5,0,6), (0,0,-2,2,1)]
+  print . maximum $ map score [ zip [a,b,c,100-a-b-c] is | a <- [1..97],
+                                                           b <- [1..97],
+                                                           c <- [1..97],
+                                                           a+b+c <= 99 ]
+
