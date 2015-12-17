66c808addbc86c5ddeb92c3677fc5cd9d9189060
Robin Schroer <sulami@peerwire.org>
2015-12-17 18:32:50 +0100

    matasano: add a scoring function for evaluating text

--- matasano/Matasano.hs
+++ matasano/Matasano.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | This module provides a lot of utility for use in the matasano crypto
 -- challenges. One of the main concerns is proper type-safe handling of
@@ -6,13 +7,15 @@
 
 module Matasano where
 
+import           Control.Arrow ((&&&), second)
 import           Data.Bits (xor)
-import           Data.Maybe (fromMaybe)
+import           Data.Maybe (fromMaybe, isJust)
 import           Data.String (IsString)
 
-import           Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy as BS
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import           Data.Hex (hex, unhex)
+import           Data.Word (Word8)
 
 -- | Describes a lazy 'ByteString' that is valid hex encoded data
 newtype HexByteString = Hex ByteString
@@ -32,8 +35,47 @@ fromHex (Hex bs) = unhex bs
 unsafeFromHex :: HexByteString -> ByteString
 unsafeFromHex (Hex bs) = fromMaybe (error "Failed to unhex") $ unhex bs
 
+-- | Check if a hex encoded bytestring is valid
+checkHex :: HexByteString -> Bool
+checkHex = isJust . fromHex
+
 -- | 'xor' two hex encoded bytestrings. Caution: Assumes valid input data
 xorByteString :: HexByteString -> HexByteString -> HexByteString
 xorByteString a b = toHex . BS.pack $
   BS.zipWith xor (unsafeFromHex a) (unsafeFromHex b)
+
+-- | Score a string based on letter-frequency compared to average English.
+-- Lower scores are better, and generally English should have a score below 0.6
+score :: ByteString -> Float
+score s = let countLetters = fromIntegral . (`BS.count` cleanedInput)
+              occurences = map (id &&& countLetters) [97..122]
+          in sum (map compare occurences) / floatLength
+  where
+    -- Remove all characters we do not account for
+    cleanedInput :: ByteString
+    cleanedInput = BS.filter (`elem` [97..122]) s
+
+    -- We need this in a couple of places
+    floatLength :: Float
+    floatLength = fromIntegral $ BS.length cleanedInput
+
+    -- The model distribution of characters in the English language, based off
+    -- an analysis of 45406 common words
+    expected :: [(Word8, Float)]
+    expected = [ (101,42689), (105,31450), (115,29639), ( 97,28965),
+                 (114,27045), (110,26975), (116,24599), (111,21588),
+                 (108,19471), ( 99,15002), (100,13849), (117,11715),
+                 (103,10339), (112,10063), (109,9803),  (104,7808),
+                 ( 98,7368),  (121,6005),  (102,4926),  (118,3971),
+                 (107,3209),  (119,3073),  (122,1631),  (120,1053),
+                 (106,727),   (113,682) ]
+
+    -- The model distribution scaled down to the length of the input
+    relativeBaseline :: [(Word8, Float)]
+    relativeBaseline = map (second (floatLength / 363645 *)) expected
+
+    -- Compare the observed character frequency with the on we would expect in
+    -- English text
+    compare :: (Word8, Float) -> Float
+    compare (w,n) = abs . (n -) . fromMaybe 0 $ lookup w relativeBaseline
 
