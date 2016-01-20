46275c40b80a377752c55d611aed81a866c15144
Robin Schroer <sulami@peerwire.org>
2016-01-20 16:53:24 +0100

    mc: set 3 part 2

--- mc/Set3.hs
+++ mc/Set3.hs
@@ -10,3 +10,13 @@ import           MCPrelude
 allPairs :: [a] -> [b] -> [(a,b)]
 allPairs l0 l1 = concatMap (\e -> zip (repeat e) l1) l0
 
+-- 302
+
+data Card = Card Int String
+
+instance Show Card where
+  show (Card x y) = show x ++ y
+
+allCards :: [Int] -> [String] -> [Card]
+allCards rs ss = concatMap (\r -> map (Card r) ss) rs
+
