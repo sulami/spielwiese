417c1361abf2dbcde8d148938a45e501b0c80c1a
Robin Schroer <sulami@peerwire.org>
2016-01-20 12:29:20 +0100

    mc: set 1 part 2

--- mc/Set1.hs
+++ mc/Set1.hs
@@ -10,4 +10,14 @@ import           MCPrelude
 fiveRands :: [Integer]
 fiveRands = take 5 . drop 1 . map fst $ iterate (rand . snd) (0, mkSeed 1)
 
+-- 102
+
+randLetter :: Seed -> (Char, Seed)
+randLetter s = let (rv,ns) = rand s
+                in (toLetter rv, ns)
+
+randString3 :: String
+randString3 = take 3 . drop 1 . map fst $
+                iterate (randLetter . snd) (' ', mkSeed 1)
+
 
