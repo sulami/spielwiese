5640840ffa9ed635c681c76fefbaae85488ce709
Robin Schroer <sulami@peerwire.org>
2016-01-20 12:29:39 +0100

    mc: set 1 part 3

--- mc/Set1.hs
+++ mc/Set1.hs
@@ -20,4 +20,21 @@ randString3 :: String
 randString3 = take 3 . drop 1 . map fst $
                 iterate (randLetter . snd) (' ', mkSeed 1)
 
+-- 103
+
+type Gen a = Seed -> (a, Seed)
+
+generalA :: (a -> a) -> Gen a -> Gen a
+generalA fn gen s = let (rv,ns) = gen s
+                    in (fn rv, ns)
+
+randEven :: Gen Integer
+randEven = generalA (*2) rand
+
+randOdd :: Gen Integer
+randOdd = generalA (+1) randEven
+
+randTen :: Gen Integer
+randTen = generalA (*10) rand
+
 
