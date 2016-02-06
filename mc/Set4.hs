b5834a2b40e0ecb724f166f27b4186537a07509d
Robin Schroer <sulami@peerwire.org>
2016-02-06 17:42:27 +0100

    mc: add some more stuff that was left over

--- mc/Set4.hs
+++ mc/Set4.hs
@@ -4,10 +4,29 @@
 module Set4 where
 
 import           MCPrelude
+import           Set2
 
 -- 403
 
 class Monad m where
   bind :: m a -> (a -> m b) -> m b
   return :: a -> m a
+
+-- 404
+
+newtype Gen a = Gen (Seed -> (a, Seed))
+
+-- (Seed -> (a,Seed)) -> (a -> (Seed -> (a,Seed))) -> Seed -> (a,Seed)
+
+instance Monad Gen where
+  bind (Gen g) f s = let (rv,ns) = g s in f ns
+
+instance Monad Maybe where
+  bind Nothing  _ = Nothing
+  bind (Just x) f = f x
+  return          = Just
+
+instance Monad [] where
+  bind     = flip concatMap
+  return x = [x]
 
