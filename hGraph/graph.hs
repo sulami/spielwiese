fbe7ddf6dc9736a0772f24abef0993c9f5996abf
Robin Schroer <sulami@peerwire.org>
2016-02-09 13:54:26 +0100

    hGraph: make some minor optimizations

--- hGraph/graph.hs
+++ hGraph/graph.hs
@@ -4,10 +4,10 @@ blocks :: String
 blocks = " ▁▂▃▄▅▆▇█"
 
 normalize :: [Int] -> [Int]
-normalize a = map (\n -> 8 * n `div` (maximum a)) a
+normalize a = map (\n -> 8 * n `div` maximum a) a
 
 visualize :: [Int] -> String
-visualize = map (\n -> blocks !! n) . normalize
+visualize = map (blocks !!) . normalize
 
 main :: IO ()
 main = visualize . map read . words <$> getLine >>= putStrLn
