67029c2dbcb321daae58c86fe94a9b85c88a6d0b
Robin Schroer <sulami@peerwire.org>
2016-12-07 15:58:20 +0100

    advent16: solve day 1 part 2

--- advent16/01.hs
+++ advent16/01.hs
@@ -29,24 +29,34 @@ getSteps (RightC n) = n
 
 type Coord = (Int, Int)
 
-walk :: Command -> Direction -> Coord -> Coord
+walk :: Command -> Direction -> Coord -> [Coord]
 walk cmd dir (x,y) = case dir of
-  North -> (x, y + getSteps cmd)
-  South -> (x, y - getSteps cmd)
-  East -> (x + getSteps cmd, y)
-  West -> (x - getSteps cmd, y)
+  North -> [(x, y + s) | s <- [1..getSteps cmd]]
+  South -> [(x, y - s) | s <- [1..getSteps cmd]]
+  East  -> [(x + s, y) | s <- [1..getSteps cmd]]
+  West  -> [(x - s, y) | s <- [1..getSteps cmd]]
 
-calculateDistance :: [Command] -> Int
-calculateDistance cmds = cd cmds North (0,0)
+distance :: Coord -> Int
+distance (x,y) = abs x + abs y
+
+path :: [Command] -> [Coord]
+path cmds = cd cmds North (0,0)
   where
-    cd :: [Command] -> Direction -> Coord -> Int
-    cd []       _   (x,y) = abs x + abs y
-    cd (c:cmds) dir pos   = let newdir = turn c dir
-                            in cd cmds newdir (walk c newdir pos)
+    cd :: [Command] -> Direction -> Coord -> [Coord]
+    cd []       _   pos = [pos]
+    cd (c:cmds) dir pos = let newdir = turn c dir
+                              steps = walk c newdir pos
+                          in pos : init steps ++ cd cmds newdir (last steps)
 
+firstDuplicate :: Eq a => [a] -> a
+firstDuplicate (x:xs) | x `elem` xs = x
+                      | otherwise   = firstDuplicate xs
+firstDuplicate []     = undefined
 
 main :: IO ()
 main = do
   cmds <- map (readCommand . unpack . strip) . split (== ',') . pack <$>
           readFile "01.input"
-  print $ calculateDistance cmds
+  let way = path cmds
+  print . distance $ last way
+  print . distance . firstDuplicate $ way
