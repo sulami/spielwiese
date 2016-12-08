cca31708d9fa48f44cd473addf98b1fddda9023e
Robin Schroer <sulami@peerwire.org>
2016-12-08 10:28:15 +0100

    advent16: add day 3 part 2

--- advent16/03.hs
+++ advent16/03.hs
@@ -1,13 +1,19 @@
 module Main where
 
-import           Data.List (sort)
+import           Data.List (sort, transpose)
 
 isValid :: [Int] -> Bool
 isValid sides = let sorted = sort sides
                 in sum (init sorted) > last sorted
 
+tripleTranspose :: [[a]] -> [[a]]
+tripleTranspose [] = []
+tripleTranspose l  = transpose (take 3 l) ++ tripleTranspose (drop 3 l)
+
 main :: IO ()
 main = do
-  indata <- map (map read . words :: String -> [Int]) . lines <$>
-    readFile "03.input"
-  print . length $ filter isValid indata
+  indata <- lines <$> readFile "03.input"
+  let part_one = map (map read . words :: String -> [Int]) indata
+      part_two = tripleTranspose part_one
+  print . length $ filter isValid part_one
+  print . length $ filter isValid part_two
