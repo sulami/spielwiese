4b82824c52d2eccf5dc7af7bb4d0b93a27593ca3
Robin Schroer <sulami@peerwire.org>
2016-12-08 12:47:46 +0100

    advent16: add day 6 part 2

--- advent16/06.hs
+++ advent16/06.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Data.List (elemIndices, maximumBy, nub, transpose)
+import           Data.List (elemIndices, maximumBy, minimumBy, nub, transpose)
 import           Data.Ord  (comparing)
 
 recover :: [String] -> String
@@ -13,7 +13,18 @@ recover input = let cols = transpose input
     numOccurences :: [Char] -> Char -> Int
     numOccurences cl c = length $ elemIndices c cl
 
+recover2 :: [String] -> String
+recover2 input = let cols = transpose input
+                  in concatMap recoverChar cols
+  where
+    recoverChar :: [Char] -> [Char]
+    recoverChar xs = [minimumBy (comparing (numOccurences xs)) $ nub xs]
+
+    numOccurences :: [Char] -> Char -> Int
+    numOccurences cl c = length $ elemIndices c cl
+
 main :: IO ()
 main = do
   indata <- lines <$> readFile "06.input"
-  print $ recover indata
+  putStrLn $ recover indata
+  putStrLn $ recover2 indata
