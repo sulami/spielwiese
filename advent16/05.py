e2fa0de8bbbc1e3760b5f8936cebad23489932ad
Robin Schroer <sulami@peerwire.org>
2016-12-10 17:21:45 +0100

    advent16: add day 5 part 2

--- advent16/05.py
+++ advent16/05.py
@@ -5,16 +5,34 @@ from itertools import count
 from hashlib import md5
 from sys import stdin
 
-def main():
-    indata = stdin.read()
+def find_password(door_id):
     password = ''
     for i in count():
-        hashed = md5((indata + str(i)).encode()).hexdigest()
+        hashed = md5((door_id + str(i)).encode()).hexdigest()
         if hashed[:5] == '00000':
             password += hashed[5]
             if len(password) == 8:
                 print(password)
                 break
+
+
+def find_password2(door_id):
+    password = list('________')
+    for i in count():
+        hashed = md5((door_id + str(i)).encode()).hexdigest()
+        if hashed[:5] == '00000':
+            pos = hashed[5]
+            if pos in '01234567' and password[int(pos)] == '_':
+                password[int(pos)] = hashed[6]
+                if '_' not in password:
+                    print(''.join(password))
+                    break
+
+
+def main():
+    indata = stdin.read()
+    find_password(indata)
+    find_password2(indata)
 
 if __name__ == '__main__':
     main()
