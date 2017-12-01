dc26159482abb245dbd6c480993106b587f24a96
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-01 15:00:14 +0000

    Gained some insight from minification

--- advent17/01.hs
+++ advent17/01.hs
@@ -19,5 +19,5 @@ matchesHalfway :: String -> String
 matchesHalfway = matchesSomething (\s -> drop (length s `div` 2) $ cycle s)
 
 matchesSomething :: (String -> String) -> String -> String
-matchesSomething trans xs = filter (/= ' ') . zipWith sameOrBlank xs $ trans xs
-  where sameOrBlank a b = if a == b then a else ' '
+matchesSomething trans xs = concat . zipWith sameOrEmpty xs $ trans xs
+  where sameOrEmpty a b = if a == b then [a] else []
