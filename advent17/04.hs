557cadc167e04c56e27a57ba439cc89579f86585
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-04 09:38:44 +0000

    AoC17/04 - part 2

--- advent17/04.hs
+++ advent17/04.hs
@@ -3,12 +3,16 @@
 
 module Main where
 
-import Data.List (nub)
+import           Data.List (nub, sort)
 
 main :: IO ()
 main = do
   input <- map words . lines <$> getContents
   print . length $ filter isValid input
+  print . length $ filter isSecure input
 
 isValid :: [String] -> Bool
 isValid phrase = phrase == nub phrase
+
+isSecure :: [String] -> Bool
+isSecure phrase = let ws = map sort phrase in ws == nub ws
