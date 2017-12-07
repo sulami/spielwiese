5f65e01acccbb1adf21e556ac8a350a9921e8a9f
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-07 12:52:45 +0000

    AoC17/07 - make it nicer

--- advent17/07.hs
+++ advent17/07.hs
@@ -12,18 +12,12 @@ data Program = Program
   , string   :: String
   } deriving Show
 
+type ProgramWeight = (Int, Int)
+
 readProgram :: String -> Program
 readProgram s = let (n:ws:_) = words s
                     w = read $ filter (`elem` ['0'..'9']) ws
                 in Program n w [] s
-
-showProgram :: Program -> String
-showProgram = showProgram' ""
-  where
-    showProgram' :: String -> Program -> String
-    showProgram' prefix p = let self = concat [prefix, name p, " (", show (weight p), ")"]
-                                children = map (showProgram' (prefix ++ " ")) $ carrying p
-                            in unlines $ self : [concat children]
 
 main :: IO ()
 main = do
@@ -36,29 +30,36 @@ main = do
 
 findRoot :: [String] -> Program
 findRoot []     = undefined
-findRoot (x:xs) = findRoot' (readProgram x) xs
+findRoot (x:xs) = findRoot' (readProgram x)
   where
-    findRoot' :: Program -> [String] -> Program
-    findRoot' p [] = p
-    findRoot' p ss = let parents = [ r | r <- ss,
-                                     name p `isInfixOf` r,
-                                     head (words r) /= name p ]
+    findRoot' :: Program -> Program
+    findRoot' p = let parents = [ r | r <- xs,
+                                  name p `isInfixOf` r,
+                                  head (words r) /= name p ]
                       in if null parents
                          then p
-                         else findRoot' (readProgram $ head parents) ss
+                         else findRoot' (readProgram $ head parents)
 
 buildTree :: Program -> [String] -> Program
 buildTree root ss = let children = [ readProgram c | c <- ss,
-                                     head (words c) `elem` map (filter (`elem` ['a'..'z'])) (drop 3 (words (string root))) ]
-                    in Program (name root) (weight root) (map (`buildTree` ss) children) (string root)
+                                     let this = head (words c),
+                                     let thisChildren = drop 3 . words $ string root,
+                                     let onlyChars = filter (`elem` ['a'..'z']),
+                                     this `elem` map onlyChars thisChildren ]
+                    in Program
+                       (name root)
+                       (weight root)
+                       (map (`buildTree` ss) children)
+                       (string root)
 
-weights :: Program -> (Int, Int)
+weights :: Program -> ProgramWeight
 weights p = if null $ carrying p
             then (weight p, weight p)
             else (weight p, weight p + (sum . map (snd . weights) $ carrying p))
 
 balanced :: Program -> Bool
-balanced p = let ws = map weights (carrying p) in all (== snd (head ws)) $ map snd ws
+balanced p = let ws = map weights (carrying p)
+             in all (== snd (head ws)) $ map snd ws
 
 trav :: Program -> [Program]
 trav root = root : concatMap trav (carrying root)
