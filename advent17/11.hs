710c4bec10265772370d01431893b53b996414e6
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-11 14:07:46 +0000

    AoC17/11 - part 11

---
+++ advent17/11.hs
@@ -0,0 +1,40 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-9.13 --install-ghc runghc
+
+module Main where
+
+data Direction = N | NE | SE | S | SW | NW
+  deriving Show
+
+type Position = (Int, Int)
+
+readDirection :: String -> Direction
+readDirection s = case s of
+  "n"  -> N
+  "ne" -> NE
+  "se" -> SE
+  "s"  -> S
+  "sw" -> SW
+  "nw" -> NW
+  _    -> error "not a direction"
+
+main :: IO ()
+main = do
+  input <- reverse . map readDirection . splitOn (== ',') <$> getLine
+  print . solve $ foldr move (0,0) input
+
+splitOn :: (a -> Bool) -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn f xs = let (this, rest) = break f xs
+               in this : splitOn f (drop 1 rest)
+
+move :: Direction -> Position -> Position
+move N (x,y)  = (x,y+1)
+move NE (x,y) = (x+1,y)
+move SE (x,y) = (x+1,y-1)
+move S (x,y)  = (x,y-1)
+move SW (x,y) = (x-1, y)
+move NW (x,y) = (x-1, y+1)
+
+solve :: Position -> Int
+solve (x,y) = abs $ maximum [x,y]
