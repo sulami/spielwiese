e99e00186ae00182cfbfe107f8fa14168dedf8ed
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-12 10:57:49 +0000

    AoC17/12 - part 2

--- advent17/12.hs
+++ advent17/12.hs
@@ -11,8 +11,8 @@ type Pair = (Int, [Int])
 main :: IO ()
 main = do
   input <- zip [0..] . map connections . lines <$> getContents
-  -- mapM_ print input
   print . length . nub $ graph 0 input
+  print . length $ groups input
 
 connections :: String -> [Int]
 connections = map read . drop 2 . words . filter (/= ',')
@@ -22,3 +22,9 @@ graph _ [] = []
 graph n xs = concat [ n : graph (fst x)  (filter ((/= n) . fst) xs) |
                       x <- [ (y, fromMaybe [] (lookup y xs)) |
                              y <- fromMaybe [] (lookup n xs) ] ]
+
+groups :: [Pair] -> [[Int]]
+groups [] = []
+groups xs = let this = graph (fst $ head xs) xs
+                rest = filter ((`notElem` this) . fst) xs
+            in this : groups rest
