98eb0c84e277fb00e70ae538c81c7a8c33a93f78
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-12-15 09:04:16 +0000

    AoC17/15 - part 2

--- advent17/15.hs
+++ advent17/15.hs
@@ -11,10 +11,13 @@ main = do
   input <- map read . lines <$> getContents :: IO [Int]
   let a = generator 16807 $ head input
       b = generator 48271 $ last input
+      pickyA = map showBinary $ filter (\x -> x `mod` 4 == 0) a
+      pickyB = map showBinary $ filter (\x -> x `mod` 8 == 0) b
   print . length . filter id . take 40000000 $ zipWith (==) a b
+  print . length . filter id . take 5000000 $ zipWith (==) pickyA pickyB
 
-generator :: Int -> Int -> [String]
-generator fac = map (drop 16 . showBinary) . iterate (generate fac)
+generator :: Int -> Int -> [Int]
+generator fac = iterate (generate fac)
 
 generate :: Int -> Int -> Int
 generate fac base = (base * fac) `rem` 2147483647
@@ -22,4 +25,4 @@ generate fac base = (base * fac) `rem` 2147483647
 showBinary :: Int -> String
 showBinary x = let bin = showIntAtBase 2 intToDigit x ""
                    prefix = replicate (32 - length bin) '0'
-               in prefix ++ bin
+               in drop 16 $ prefix ++ bin
