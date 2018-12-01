d78458f9c516aa2f65aa82044ea233dbc460d344
Robin Schroer <sulami@peerwire.org>
2018-12-01 10:36:57 +0100

    aoc18: day 1

---
+++ advent18/src/advent18/01.clj
@@ -0,0 +1,25 @@
+(ns advent18.01
+  (:require [clojure.string :as str]))
+
+(def input
+  (->> "01.input"
+       slurp
+       str/split-lines
+       (map read-string)))
+
+(def part-one
+  (reduce + input))
+
+(defn add-and-compare
+  [[seen curr] coll]
+  (let [next-number (-> coll first (+ curr))]
+    (if (seen next-number)
+      next-number
+      (recur [(conj seen next-number) next-number] (rest coll)))))
+
+(def part-two
+  (add-and-compare [#{} 0] (cycle input)))
+
+(defn -main []
+  (println part-one)
+  (println part-two))
