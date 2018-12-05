33952f255403f96acaa606ad0e7857ff479601af
Robin Schroer <sulami@peerwire.org>
2018-12-05 11:37:24 +0100

    aoc18: day 5 part 1

---
+++ advent18/src/advent18/05.clj
@@ -0,0 +1,32 @@
+(ns advent18.05
+  (:require [clojure.string :as str]))
+
+(def input
+  (->>  "../../resources/05.input"
+        slurp
+        str/trim))
+
+(defn f [acc coll]
+  (if (empty? coll)
+    acc
+    (let [head (first coll)
+          tail (rest coll)]
+      (if (empty? tail)
+        (conj acc head)
+        (if (and (= (str/upper-case head) (str/upper-case (first tail)))
+                 (not= head (first tail)))
+          (recur acc (rest tail))
+          (recur (conj acc head) tail))))))
+
+(defn iterate-until-unchanged [f x]
+  (->> x
+       (iterate f)
+       (reduce #(if (= %1 %2) (reduced %1) %2))))
+
+(def part-one
+  (->> input
+       (iterate-until-unchanged (partial f []))
+       count))
+
+(defn -main []
+  (println part-one))
